2a378632e3a548458545a2c0adb3cfce9c468689
Jeremie DECOCK <jd.jdhp@gmail.com>
2011-10-15 19:34:15 +0200

    Add boost_random demos.

---
+++ boost_random/rand_normal.cc
@@ -0,0 +1,30 @@
+#include <boost/random.hpp>
+#include <boost/random/normal_distribution.hpp>
+#include <ctime>
+#include <iostream>
+
+const int num_it = 100000;
+const double min = 1.0;
+const double max = 10.0;
+const double mu = 0.0;
+const double sigma = 1.0;
+
+main() {
+
+    // Pseudo-random number generator
+    //boost::mt19937 rng;               // deterministic
+    boost::mt19937 rng(std::time(NULL));
+
+    // Random number distribution
+    boost::normal_distribution<> distrib(mu, sigma);
+
+    // Generator (wrapper)
+    boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > random_generator(rng, distrib);
+
+    for(int i=0 ; i<num_it ; i++) {
+        double x = random_generator();
+        double y = random_generator();
+        std::cout << x << " " << y << std::endl;
+    }
+
+}
---
+++ boost_random/rand_uniform_real.cc
@@ -0,0 +1,31 @@
+#include <boost/random.hpp>
+#include <boost/random/uniform_real.hpp>
+#include <ctime>
+#include <iostream>
+
+const int num_it = 10;
+const double min = 1.0;
+const double max = 10.0;
+
+main() {
+
+    // Pseudo-random number generator
+    //boost::mt19937 rng;               // deterministic
+    boost::mt19937 rng(std::time(NULL));
+
+    // Random number distribution (that maps to min..max)
+    boost::uniform_real<> distrib(min, max);
+
+    // Generator (wrapper)
+    boost::variate_generator<boost::mt19937&, boost::uniform_real<> > random_generator(rng, distrib);
+
+    for(int i=0 ; i<num_it ; i++) {
+        //double x = distrib(rng);
+        double x = random_generator();
+        std::cout << x << " ";
+    }
+
+    std::cout << std::endl;
+
+}
+
