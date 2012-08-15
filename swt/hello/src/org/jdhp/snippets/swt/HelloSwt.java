f1586079457edac70e40d0c6b87f761901228bcb
Jérémie Decock <jeremie.decock@inria.fr>
2012-08-15 12:26:42 +0200

    Move 'hello_swt'.

---
+++ swt/hello/src/org/jdhp/snippets/swt/HelloSwt.java
@@ -0,0 +1,41 @@
+package org.jdhp.snippets.swt;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class HelloSwt {
+
+    public static void main(String [] args) {
+        Display display = new Display();
+
+        // Make the window
+        Shell shell = new Shell(display);
+        shell.setLayout(new GridLayout(1, false));
+
+        shell.setText("Hello SWT");
+        shell.setMinimumSize(400, 350);
+        shell.setSize(640, 480);
+
+        // Add a widget
+        Label label = new Label(shell, SWT.NONE);
+        label.setLayoutData(new GridData(GridData.FILL_BOTH));
+        label.setText("Hello World");
+        label.pack();
+
+        // Render the window
+        shell.pack();
+        shell.open();
+
+        while(!shell.isDisposed()) {
+            if(!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+
+        display.dispose();
+    }
+}
