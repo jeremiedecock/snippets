00cd2891fc45e2b2c1a161a00f35a3404859becb
Jeremie DECOCK <jd.jdhp@gmail.com>
2012-09-22 14:05:02 +0200

    Add two 'python/argparse' snippets.

---
+++ python/argparse/hello2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2012 Jérémie DECOCK (http://www.jdhp.org)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import argparse
+
+DESCRIPTION = "An argparse snippet."
+EPILOG = "Please report bugs to <jd.jdhp@gmail.com>."
+VERSION = "2.0"
+
+def main():
+    """Main function"""
+
+    parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG)
+    parser.add_argument("filenames", nargs='+', type=file, metavar="FILE", help="file to read")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s " + VERSION)
+    args = parser.parse_args()
+
+    for fd in args.filenames:
+        print fd.read()
+        fd.close()
+
+if __name__ == '__main__':
+    main()
+
