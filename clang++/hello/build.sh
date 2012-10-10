475aec6e09f52c21ec6534541122cbdacd0a88e2
Jeremie DECOCK <jd.jdhp@gmail.com>
2012-10-10 12:09:53 +0200

    Remove some useless snippets (they should be in OPC, not here).

--- clang++/hello/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CC=clang++
-
-$CC -Wall -pedantic -g -c hello.cc
-$CC -o hello hello.o
-
--- clang/hello/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CC=clang
-
-$CC -Wall -pedantic -g -c hello.c
-$CC -o hello hello.o
-
