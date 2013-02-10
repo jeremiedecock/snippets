845d6a27b9364f466559f5364ba0d1a09f6b3a0b
Jeremie DECOCK <jd.jdhp@gmail.com>
2013-02-10 12:53:22 +0100

    Add and update a snippets.

---
+++ c/unix/group.c
@@ -0,0 +1,51 @@
+/* 
+ * Fork: display and modify the real (GID) and effective (EGID) group ID of the
+ *       process.
+ *
+ * Copyright (c) 2013 Jérémie Decock
+ *
+ * Usage: gcc group.c
+ * See "man 2 getgid", "man 2 getegid" and "man 2 setgid" for more info
+ *
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char * argv[])
+{
+    fprintf(stdout, "Run the two following commands as root to test this program:\n"
+                    "chown 1000.1000 %s\n"
+                    "chmod g+s %s\n\n", argv[0], argv[0]);
+
+    gid_t real_group_id = getgid();
+    gid_t effective_group_id = getegid();
+
+    fprintf(stdout, "Real group ID of the process (GID) = %u\n", getgid());
+    fprintf(stdout, "Effective group ID of the process (EGID) = %u\n", getegid());
+
+    // Lost privileges: Effective GID -> Real GID
+
+    int result;
+
+    result = setgid(real_group_id);
+    fprintf(stdout,
+            "\nSet EGID to the GID (lose privileges): setgid(%u) = %d [%s]\n\n",
+            real_group_id, result, result==0 ? "success" : "failure");
+
+    fprintf(stdout, "Real group ID of the process (GID) = %u\n", getgid());
+    fprintf(stdout, "Effective group ID of the process (EGID) = %u\n", getegid());
+
+    // Gain back privileges: Real GID -> Effective GID
+
+    result = setgid(effective_group_id);
+    fprintf(stdout,
+            "\nSet EGID to the former EGID (gain back privileges): setgid(%u) = %d [%s]\n\n",
+            effective_group_id, result, result==0 ? "success" : "failure");
+
+    fprintf(stdout, "Real group ID of the process (GID) = %u\n", getgid());
+    fprintf(stdout, "Effective group ID of the process (EGID) = %u\n", getegid());
+
+
+    return 0;
+}
--- c/unix/user.c
+++ c/unix/user.c
@@ -14,7 +14,9 @@
 
 int main(int argc, char * argv[])
 {
-    fprintf(stdout, "Run the two following commands as root to test this program:\nchown 1000.1000 %s\nchmod u+s %s\n\n", argv[0], argv[0]);
+    fprintf(stdout, "Run the two following commands as root to test this program:\n"
+                    "chown 1000.1000 %s\n"
+                    "chmod u+s %s\n\n", argv[0], argv[0]);
 
     uid_t real_user_id = getuid();
     uid_t effective_user_id = geteuid();
@@ -24,14 +26,22 @@ int main(int argc, char * argv[])
 
     // Lost privileges: Effective UID -> Real UID
 
-    fprintf(stdout, "\nSet EUID to the UID: setuid(%u) = %d\n\n", real_user_id, setuid(real_user_id));
+    int result;
+
+    result = setuid(real_user_id);
+    fprintf(stdout,
+            "\nSet EUID to the UID (lose privileges): setuid(%u) = %d [%s]\n\n",
+            real_user_id, result, result==0 ? "success" : "failure");
 
     fprintf(stdout, "Real user ID of the process (UID) = %u\n", getuid());
     fprintf(stdout, "Effective user ID of the process (EUID) = %u\n", geteuid());
 
     // Gain back privileges: Real UID -> Effective UID
 
-    fprintf(stdout, "\nSet EUID to the former EUID: setuid(%u) = %d\n\n", effective_user_id, setuid(effective_user_id));
+    result = setuid(effective_user_id);
+    fprintf(stdout,
+            "\nSet EUID to the former EUID (gain back privileges): setuid(%u) = %d [%s]\n\n",
+            effective_user_id, result, result==0 ? "success" : "failure");
 
     fprintf(stdout, "Real user ID of the process (UID) = %u\n", getuid());
     fprintf(stdout, "Effective user ID of the process (EUID) = %u\n", geteuid());
