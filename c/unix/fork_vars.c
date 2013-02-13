30d44d14620b315d116b5740f6cb3029f2475424
Jeremie DECOCK <jd.jdhp@gmail.com>
2013-02-13 08:40:59 +0100

    Add a snippet.

---
+++ c/unix/fork_vars.c
@@ -0,0 +1,53 @@
+/* 
+ * Fork: fork the process and display resulting PID and PPID.
+ *
+ * Copyright (c) 2013 Jérémie Decock
+ *
+ * Usage: gcc fork1.c
+ * See "man 2 fork" for more info
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int global_var = 0;
+
+int main(int argc, char * argv[])
+{
+    int local_var = 0;
+
+    pid_t proc_id = fork();
+
+    if(proc_id == -1) {
+
+        // Error...
+        fprintf(stderr, "Fork failure\n");
+        exit(1);
+
+    } else if(proc_id == 0) {
+
+        // Child process
+        
+        global_var++;
+        local_var++;
+
+        fprintf(stdout, "Child (PID=%ld): global_var=%d local_var=%d\n", (long) getpid(), global_var, local_var);
+
+        exit(0);
+
+    } else {
+
+        // Parent process
+
+        global_var--;
+        local_var--;
+
+        fprintf(stdout, "Parent (PID=%ld): global_var=%d local_var=%d\n", (long) getpid(), global_var, local_var);
+
+        // Wait the end of the child process
+        wait(NULL);
+        exit(0);
+    }
+}
