a14e802d54999038e19903d9c7464f7cd79a260c
Jeremie DECOCK <jd.jdhp@gmail.com>
2013-02-24 23:14:45 +0100

    Minor update.

--- python/numpy/random/multivariate_normal.py
+++ python/numpy/random/multivariate_normal.py
@@ -8,11 +8,16 @@ from mpl_toolkits.mplot3d import axes3d
 
 
 mu = np.array([0., 0.])
-cov = np.array([[1., 0.], [0., 1.]])
+cov = np.array([[1., 0.],
+                [1., 1.]])
 num_points = 10000
 
 x = np.random.multivariate_normal(mu, cov, num_points)
 
 plt.plot(x[:,0], x[:,1], ".")
+
+plt.xlim(-5, 5)
+plt.ylim(-5, 5)
+
 plt.show()
 
