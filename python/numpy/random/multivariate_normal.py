0195d05fb443ec9025242a6f68e51ad1d8327966
Jeremie DECOCK <jd.jdhp@gmail.com>
2013-02-24 22:09:54 +0100

    Add a snippet (python/numpy/random/multivariate_normal).

---
+++ python/numpy/random/multivariate_normal.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+            
+import math 
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+
+mu = np.array([0., 0.])
+cov = np.array([[1., 0.], [0., 1.]])
+num_points = 10000
+
+x = np.random.multivariate_normal(mu, cov, num_points)
+
+plt.plot(x[:,0], x[:,1], ".")
+plt.show()
+
