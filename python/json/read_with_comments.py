344e1e1ddc19280525d0b46553cc3220fa822770
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-01-03 22:36:45 +0100

    Add some snippets (python/json).

---
+++ python/json/read_with_comments.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2014 Jérémie DECOCK (http://www.jdhp.org)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# See http://docs.python.org/2/library/json.html#module-json
+
+import json
+
+def load_json_with_comments(filename):
+    data = None
+
+    with open("test_with_comments.json", "r") as fd:
+        pos = 0
+        for line in fd.readlines():
+            if line.lstrip().startswith("//"):
+                pos += len(line)
+            else:
+                break
+
+        fd.seek(pos)
+        data_str = fd.read()
+
+        data = json.loads(data_str)
+
+    return data
+
+
+def main():
+    """Main function"""
+
+    data = load_json_with_comments("test_with_comments.json")
+    print data
+
+if __name__ == '__main__':
+    main()
