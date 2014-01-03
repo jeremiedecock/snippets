525dc8ee7f5cc131fab09a989dde8b22657746c9
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-01-03 22:36:05 +0100

    Add some snippets (python/config_parser).

---
+++ python/config_parser/python2/write_ini.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2014 Jérémie DECOCK (http://www.jdhp.org)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# See http://docs.python.org/2/library/configparser.html
+
+import ConfigParser as cp
+
+def main():
+    """Main function"""
+
+    config = cp.RawConfigParser()
+
+    config.add_section("Section1")
+    config.set("Section1", "name1", "value1")
+    config.set("Section1", "name2", "2")
+    config.set("Section1", "name3", "3.14")
+    config.set("Section1", "name4", "True")
+
+    with open('test.ini', 'wb') as fd:
+        config.write(fd)
+
+if __name__ == '__main__':
+    main()
