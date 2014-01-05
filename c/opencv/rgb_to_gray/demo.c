4c0258aecb1cc0948e731396fc4f02c5509c1dba
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-01-05 13:48:45 +0100

    Add a snippet (c/opencv).

---
+++ c/opencv/rgb_to_gray/demo.c
@@ -0,0 +1,77 @@
+/* 
+ * OpenCV - Transform video
+ *
+ * Copyright (c) 2014 Jérémie Decock
+ *
+ * Required: opencv library (Debian: aptitude install libopencv-dev)
+ * Usage: gcc demo.c $(pkg-config --cflags --libs opencv)
+ *
+ * See: Oreilly's book "Learning OpenCV" (first edition) p.24 and see chapter 6 for details about transformations
+ *
+ */
+
+#include <stdio.h>
+#include <cv.h>
+#include <highgui.h>
+
+static const int frame_delay_ms = 40; // (40ms -> 25fps)
+
+int main(int argc, char *argv[])
+{
+    int camera_id = -1;
+
+    if(argc == 2) {
+        if(0 == strcmp(argv[1], "-h")) {
+            fprintf(stderr, "Usage: %s [CAMERA_ID]\n", argv[0]);
+            exit(0);
+        } else {
+            camera_id = atoi(argv[1]);
+        }
+    }
+
+    CvCapture* capture = cvCreateCameraCapture(camera_id);  // camera id number (-1=any camera)
+
+    if(capture == NULL) {
+        fprintf(stderr, "Error: cannot find any camera.\n");
+        exit(1);
+    } else {
+        fprintf(stdout, "Press Esc to close the window.\n");
+    }
+
+    
+    // INIT
+    
+    cvNamedWindow("Input", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("Output", CV_WINDOW_AUTOSIZE);
+
+    IplImage* in_frame = cvQueryFrame(capture);
+    CvSize frame_size = cvGetSize(in_frame);
+
+    IplImage* out_frame_gray = cvCreateImage(frame_size, IPL_DEPTH_8U, 1); // 1 channel (black and white)
+
+
+    // MAIN LOOP
+    
+    while(1) {
+        in_frame = cvQueryFrame(capture);
+        if(in_frame == NULL) break;
+
+        cvCvtColor(in_frame, out_frame_gray, CV_BGR2GRAY); // RGB -> Gray
+
+        cvShowImage("Input", in_frame);         // display the frame
+        cvShowImage("Output", out_frame_gray);  // display the frame
+
+        char c = cvWaitKey(frame_delay_ms); // wait for 40ms (40ms -> 25fps)
+        if(c==27) break;                    // 27 = Esc key
+    }
+
+
+    // RELEASE POINTERS
+    
+    cvReleaseCapture(&capture);
+    cvReleaseImage(&out_frame_gray);
+    cvDestroyWindow("Input");
+    cvDestroyWindow("Output");
+
+    return 0;
+}
