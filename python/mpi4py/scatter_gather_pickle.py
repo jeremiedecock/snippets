0fb2117027eb33115a857364f5c27dc8532441d9
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-03-24 15:26:34 +0100

    Add scatter_gather_pickle.py in python/mpi4py.

---
+++ python/mpi4py/scatter_gather_pickle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2014 Jérémie DECOCK (http://www.jdhp.org)
+
+# run:
+#   mpirun -np 4 python scatter_gather_pickle.py | sort
+#     or
+#   mpiexec -n 4 python scatter_gather_pickle.py | sort
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+if rank == 0:
+    data = ['x' + str(i) for i in range(size)]
+else:
+    data = None
+
+# SCATTER
+
+print "[1. before scatter] process", rank, ":", data
+
+data = comm.scatter(data, root=0)
+
+print "[2. after scatter] process", rank, ":", data
+
+# PROCESS DATA
+
+data = data.replace('x', 'y')
+
+# GATHER
+
+print "[3. before gather] process", rank, ":", data
+
+data = comm.gather(data, root=0)
+
+print "[4. after gather] process", rank, ":", data
+
