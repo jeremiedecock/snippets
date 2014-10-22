54aae5f17c8cc4c34bf5099fc331085632eb6f68
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-10-22 15:24:12 +0200

    Add a snippet (c++/hello_cpp11).

---
+++ cpp/hello_cpp11/hello.cc
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2014 Jérémie DECOCK <jd.jdhp@gmail.com> (www.jdhp.org)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Usage:
+ * - Gcc >= 4.7: g++ -std=c++11 chrono.cc
+ * - Gcc  < 4.7: g++ -std=c++0x chrono.cc
+ */
+
+#include <iostream>
+
+int main(int argv, char ** argc) {
+
+    int tab[5] = {1, 2, 3, 4, 5};
+    for(int &x : tab) {
+        std::cout << x << std::endl;
+    }
+
+    return 0;
+}
