c290dec5bec1c18e112241acdba3f9beeeaec8b6
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-10-22 14:54:19 +0200

    Add an 'hello world' with makefile for C and C++.

--- c/hello/hello.c
+++ c/hello/hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, world!\n");
+int main(int argv, char ** argc) {
+
+    printf("Hello\n");
+
     return 0;
 }
---
+++ cpp/hello/hello.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argv, char ** argc) {
+
+    std::cout << "Hello" << std::endl;
+
+    return 0;
+}
