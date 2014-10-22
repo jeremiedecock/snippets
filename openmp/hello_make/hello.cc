af53b5e11f950294e9d2de1cc716e1f83e2e9824
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-10-22 13:12:44 +0200

    Add documentation.

--- openmp/hello_make/hello.cc
+++ openmp/hello_make/hello.cc
@@ -1,9 +1,18 @@
 /*
  * A very basic demo of OpenMP.
  *
- * USAGE:
+ * USAGE (GCC):
  *    g++ -fopenmp -c hello.cc
  *    g++ -fopenmp hello.o
+ *
+ * In order to use N cores, the shell environment variable OMP_NUM_THREADS
+ * should be set (at execution time not at compilation time):
+ *    export OMP_NUM_THREADS=N
+ *
+ * For instance,
+ *    export OMP_NUM_THREADS=2
+ *    ./hello
+ * will use 2 cores.
  */
 
 #include <iostream>
