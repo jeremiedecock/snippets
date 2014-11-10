a791317d3cb6abd52eb8a027612e93a881a1372f
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-11-10 16:20:20 +0100

    Misc updates.

--- bash/edit_file_with_sed.sh
+++ bash/edit_file_with_sed.sh
@@ -2,5 +2,6 @@
 
 # Copyright (c) 2014 Jérémie DECOCK (http://www.jdhp.org)
 
-sed -i "s/^option off$/option on/" test.txt
+sed -i "s/^OPTION1=OFF$/OPTION=ON/" test.txt
+sed -i "s/^#OPTION2=\"\"$/OPTION2=\"blablabla\"/" test.txt
 
