6c5f04d4d2eb6cfb8bf438503c235573e44935cc
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-11-12 00:02:40 +0100

    Improve OpenSCAD render script.

--- openscad/render.sh
+++ openscad/render.sh
@@ -1,18 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 # Antialiased OpenSCAD rendering
 
 # See http://talpadk.wordpress.com/2014/02/21/antialiased-openscad-rendering/
 
-for FILE in $@
+for FILE in "$@"
 do
-    if [ -f "${FILE}" ]
+    # http://stackoverflow.com/questions/407184/how-to-determine-file-type-in-bash-script
+    if [[ "$FILE" == *.scad ]]
     then
         ls "${FILE}"
-        PNG_FILE="${FILE}.png"
+        PNG_FILE="$(basename "${FILE}" .scad).png"
         openscad -o "${PNG_FILE}" --imgsize=2048,2048 "${FILE}"
-        convert "${PNG_FILE}" -resize 512x512 "${PNG_FILE}"
+
+        if [ -s "${PNG_FILE}" ]
+        then
+            convert "${PNG_FILE}" -resize 512x512 "${PNG_FILE}"
+        else
+            rm -v "${PNG_FILE}"
+        fi
     else
-        echo "Error: ${FILE} is not a regular file."
+        echo "Error: ${FILE} is not an OpenSCAD file."
     fi
 done
