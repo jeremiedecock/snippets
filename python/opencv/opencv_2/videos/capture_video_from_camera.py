08573c7c6eb20c7cf168d38a6566757b08fed6d9
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-06-19 15:35:35 +0200

    Add a snippet (Python OpenCV).

---
+++ python/opencv/opencv_2/videos/capture_video_from_camera.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Jérémie DECOCK (http://www.jdhp.org)
+
+"""
+OpenCV - Read image: read an image given in arguments
+
+Required: opencv library (Debian: aptitude install python-opencv)
+
+See: https://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html#capture-video-from-camera
+"""
+
+from __future__ import print_function
+
+import cv2 as cv
+
+def main():
+
+    device_number = 0
+    video_capture = cv.VideoCapture(device_number)
+
+    print("Press Q to quit.")
+
+    while(True):
+        # Capture frame-by-frame.
+
+        # 'ret' is a boolean ('True' if frame is read correctly, 'False' otherwise).
+        # 'img_np' is an numpy array.
+        ret, img_np = video_capture.read()
+
+        # Display the resulting frame
+        cv.imshow('video capture snippet', img_np)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video_capture.release()
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
