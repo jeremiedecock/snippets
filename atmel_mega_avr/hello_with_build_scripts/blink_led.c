06530024a07b26e5e318f6f526b1074b40eaed07
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-06-22 13:44:00 +0200

    Update comments.

--- atmel_mega_avr/hello_with_build_scripts/blink_led.c
+++ atmel_mega_avr/hello_with_build_scripts/blink_led.c
@@ -5,13 +5,14 @@ int main(void) {
 
     // On Arduino Duemilanove, the embeded LED is on pin D13 (PB5/SCK with AVR notation)
 
-    DDRB = (1<<DDB5);         // Data Direction Register B: pin5 is an output
+    DDRB = (1<<DDB5);         // DDRB is the Data Direction Register B: we set pin5 (DDB5) as an output
+                              // DDB5 is just a constant equals to 5 (DDB3=3, DDC3=3, DDD3=3, ...)
 
-    PORTB &= ~(1<<DDB5);      // Turn off DDB5 (AND NOT)
+    PORTB &= ~(1<<DDB5);      // Turn off pin 5 (DDB5)
 
     while(1) {
-        PORTB ^= (1<<DDB5);   // Toggle DDB5   (XOR)
-        _delay_ms(1000);       // wait
+        PORTB ^= (1<<DDB5);   // Toggle pin5 (DDB5) with the XOR operator
+        _delay_ms(1000);      // wait 1000ms
     }
 
     return(0);                // This line is never reached...
