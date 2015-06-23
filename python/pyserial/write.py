c4c422e0d4bdf4a0273ac5e5f27bd37c58ab3f61
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-06-23 15:30:28 +0200

    Add 2 snippets (Python pyserial).

---
+++ python/pyserial/write.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Jeremie Decock (http://www.jdhp.org)
+
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import argparse
+import serial
+import time
+
+def main():
+
+    # PARSE OPTIONS
+
+    parser = argparse.ArgumentParser(description='A pyserial snippet.')
+
+    parser.add_argument("--baudrate", "-b",  help="The baudrate speed (e.g. 9600)", metavar="INTEGER", type=int, default=9600)
+    parser.add_argument("--timeout", "-t",  help="The timeout value for the connection", metavar="FLOAT", type=float, default=0.1)
+    parser.add_argument("--port", "-p",  help="The serial device to connect with (e.g. '/dev/ttyUSB0' for Unix users)", metavar="STRING", default="/dev/ttyUSB0")
+    args = parser.parse_args()
+
+    # CONNECT TO THE SERIAL PORT
+
+    serial_connection = serial.Serial(port=args.port,
+                                      baudrate=args.baudrate,
+                                      timeout=args.timeout,
+                                      bytesize=serial.EIGHTBITS,
+                                      parity=serial.PARITY_NONE,
+                                      stopbits=serial.STOPBITS_ONE)
+
+    serial_connection.flushOutput()
+
+    # WRITE DATA
+
+    data_byte_array = bytearray("Hello")
+
+    while(True):
+        time.sleep(0.1)
+
+        serial_connection.write(data_byte_array)
+
+
+if __name__ == '__main__':
+    main()
