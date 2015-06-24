758c3ba77e01a5259e9f27f714f880bacbfb50c8
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-06-24 22:53:17 +0200

    Add a snippet (Atmel AVR).

---
+++ atmel_mega_avr/pwm_timer_method/pwm.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2015 Jérémie DECOCK <jd.jdhp@gmail.com> (www.jdhp.org)
+ */
+
+#include <avr/io.h>           // Defines pins, ports, etc
+#include <util/delay.h>       // Functions to waste time
+
+int main(void) {
+
+    // INIT PB5 AS OUTPUT (LED) /////////////////////////////////////
+    
+    DDRB = (1 << DDB3);         // DDRB is the Data Direction Register B: we set pin PB3 (DDB3) as an output
+
+
+    // INIT TIMER 2 (with the 8 bits counter) ///////////////////////
+    
+    // Mode 3: Fast PWM (cf. datasheet p153 and 156), TOP=0xFF
+    TCCR2A |= (1 << WGM20);
+    TCCR2A |= (1 << WGM21);  // Waveform generation mode
+
+    // Frequency
+    TCCR2B |= (1 << CS21);   // Setup PWM Freq = F_CPU/8/256
+
+    // Output mode (setup PWM output on OCR2A)
+    TCCR2A |= (1 << COM2A1); // Clear OC2A (PB3) on compare match, set OC2A at BOTTOM
+
+
+    // EVENT LOOP ///////////////////////////////////////////////////
+    
+    uint8_t brightness = 0;
+
+    while(1) {
+        OCR2A = brightness;
+        brightness++;
+
+        _delay_ms(10);
+    }
+
+    return(0);
+}
