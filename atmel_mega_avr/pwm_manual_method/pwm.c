d3a4f2c357df86732a6fb0eefc63a6513d2d9124
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-06-24 21:23:39 +0200

    Add a snippet (Atmel AVR).

---
+++ atmel_mega_avr/pwm_manual_method/pwm.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2015 Jérémie DECOCK <jd.jdhp@gmail.com> (www.jdhp.org)
+ */
+
+#include <avr/io.h>           // Defines pins, ports, etc
+#include <util/delay.h>       // Functions to waste time
+
+// LED delay in microseconds
+#define LED_DELAY 20
+
+void pwm_pb5(uint8_t brightness) {
+
+    PORTB |= (1<<DDB5);               // Turn on the pin
+
+    uint8_t i;
+    for(i = 0 ; i < 255 ; i++) {
+        if(i >= brightness) {         // Once it's been on long enough
+            PORTB &= ~(1 << DDB5);    // Turn off the pin
+        }
+        _delay_us(LED_DELAY);
+    }
+}
+
+int main(void) {
+
+    DDRB = (1<<DDB5);         // DDRB is the Data Direction Register B: we set pin5 (DDB5) as an output
+
+    uint8_t brightness = 0;
+
+    while(1) {
+        pwm_pb5(brightness);
+        brightness++;
+    }
+
+    return(0);
+}
