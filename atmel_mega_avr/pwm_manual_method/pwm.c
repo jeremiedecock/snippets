142f69180c831bbbc8e7e833c15c9410e18568b0
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-06-25 13:54:39 +0200

    Misc update.

--- atmel_mega_avr/pwm_manual_method/pwm.c
+++ atmel_mega_avr/pwm_manual_method/pwm.c
@@ -13,7 +13,7 @@ void pwm_pb5(uint8_t brightness) {
     PORTB |= (1<<DDB5);               // Turn on the pin
 
     uint8_t i;
-    for(i = 0 ; i < 255 ; i++) {      // PWM frequency = 256 ; "Duty cytle" = brightness
+    for(i = 0 ; i < 255 ; i++) {      // PWM frequency = 256 ; "Duty cycle" = brightness
         if(i >= brightness) {         // Once it's been on long enough
             PORTB &= ~(1 << DDB5);    // Turn off the pin
         }
