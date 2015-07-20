fdebb6617f341d803a6d20b0bf3f24ded276e8b3
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:36:36 +0200

    Complete a snippet.

--- javascript/ecmascript/array.js
+++ javascript/ecmascript/array.js
@@ -1,5 +1,5 @@
 /*
- * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Types_et_grammaire
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Collections_index%C3%A9es#M%C3%A9thodes_des_tableaux
  */
 
 var tab1 = ["one", "two", "three"];
@@ -8,4 +8,30 @@ var tab2 = [1, "two", 3.0];
 print(tab1);
 print(tab2);
 
+print(tab1.length);
+
 print(tab1[0]);
+
+print(tab1.concat(tab2))
+
+print(tab1.join('---'))
+
+tab1.push("four")
+print(tab1);
+
+tab1.pop()
+print(tab1);
+
+tab1.shift("four")
+print(tab1);
+
+tab1.unshift("five")
+print(tab1);
+
+print(tab1.slice(1,3));
+
+tab1.reverse()
+print(tab1);
+
+tab1.sort()
+print(tab1);
