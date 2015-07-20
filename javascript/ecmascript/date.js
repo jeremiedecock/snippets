d43ecb9503b7d10374495030a86f13b06154ee81
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:26:01 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/date.js
@@ -0,0 +1,11 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Nombres_et_dates#L%27objet_Date
+ */
+
+var time = new Date();
+var hours   = time.getHours();
+var minutes = time.getMinutes();
+var seconds = time.getSeconds();
+
+print(time)
+print(hours, minutes, seconds)
