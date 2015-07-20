a80b74c0b8ee8d42734b10fab7459bbfded94a7a
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:25:49 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/for_of.js
@@ -0,0 +1,12 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Boucles_et_it%C3%A9ration
+ */
+
+// This script requires ECMAScript 6 or above.
+
+var tab = ["one", "two", "three"];
+
+for(var i of tab) {
+    print(i);
+}
+
