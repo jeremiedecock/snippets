ce3be75d837628049119f544111b72669746373b
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:23:26 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/types.js
@@ -0,0 +1,12 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Types_et_grammaire
+ */
+
+var v1 = true;   // or "false"
+var v2 = null;
+var v3 = undefined;
+var v4 = 3;
+var v5 = 3.14;
+var v6 = "Hello";
+
+print(v1, v2, v3, v4, v5, v6);
