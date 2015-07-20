6f7e0c5882787eb806c680456d9669ff751bbda2
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 17:23:32 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/var_and_const.js
@@ -0,0 +1,11 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Types_et_grammaire
+ */
+
+var v1 = 3;
+let v2 = 5;
+const c1 = "Hello, world!"
+
+print(v1);
+print(v2);
+print(c1);
