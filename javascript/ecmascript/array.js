0b9e4cf72fafa336280fa4c0981a239cc5db9340
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:25:19 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/array.js
@@ -0,0 +1,11 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Types_et_grammaire
+ */
+
+var tab1 = ["one", "two", "three"];
+var tab2 = [1, "two", 3.0];
+
+print(tab1);
+print(tab2);
+
+print(tab1[0]);
