ca0115809f26256e77c8b20b2f698510d0022872
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:24:51 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/if_then_else.js
@@ -0,0 +1,14 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Contr%C3%B4le_du_flux_Gestion_des_erreurs
+ */
+
+var x = 3.14;
+
+if(x == 0) {
+    print("x=0");
+} else if(x < 0) {
+    print("x<0");
+} else {
+    print("x>0");
+}
+
