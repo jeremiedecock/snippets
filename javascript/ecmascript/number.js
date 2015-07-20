80496e54c14f0ce4c49026814fd11437a7391ede
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:24:32 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/number.js
@@ -0,0 +1,21 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Nombres_et_dates#L%27objet_Number
+ */
+
+print("MAX_VALUE", Number.MAX_VALUE)
+print("MIN_VALUE", Number.MIN_VALUE)
+print("POSITIVE_INFINITY", Number.POSITIVE_INFINITY)
+print("NEGATIVE_INFINITY", Number.NEGATIVE_INFINITY)
+print("NaN", Number.NaN)
+print("MIN_SAFE_INTEGER", Number.MIN_SAFE_INTEGER)
+print("MAX_SAFE_INTEGER", Number.MAX_SAFE_INTEGER)
+
+// TODO
+//
+//x = 3.14
+//
+//print("isFinite()", Number.isFinite(x))
+//print("isInteger()", Number.isInteger(x))
+//print("isNaN()", Number.isNaN(x))
+//print("isSafeInteger()", Number.isSafeInteger(x))
+
