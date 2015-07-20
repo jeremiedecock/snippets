e20499517215c7d27fe1f2d1d4453c575ec1c53f
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 20:06:42 +0200

    Complete a snippet.

--- javascript/ecmascript/function.js
+++ javascript/ecmascript/function.js
@@ -10,3 +10,13 @@ var v1 = "Hello";
 
 print_var(v1);
 
+///////////////////////////////////////////////////////////////////////////////
+
+function factorial(n){
+    if ((n == 0) || (n == 1))
+        return 1;
+    else
+        return (n * factorial(n - 1));
+}
+
+print(factorial(4));
