a567eca01cc8e68dc32a0de29e9c661b80cc0b9b
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:23:14 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/var_and_let.js
@@ -0,0 +1,12 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Types_et_grammaire
+ */
+
+{
+    var v1 = 3;
+    let v2 = 5;
+}
+
+print(v1); // Ok
+print(v2); // Error: v2 is undefined
+
