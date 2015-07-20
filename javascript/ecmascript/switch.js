762410d48abe6fff15d409a92d56187a921705be
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:24:59 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/switch.js
@@ -0,0 +1,19 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Contr%C3%B4le_du_flux_Gestion_des_erreurs
+ */
+
+var str = "banana";
+
+switch(str) {
+    case "orange":
+        print("orange");
+        break;
+    case "apple":
+        print("pomme");
+        break;
+    case "banana":
+        print("banane");
+        break;
+    default:        
+        print("default");
+}
