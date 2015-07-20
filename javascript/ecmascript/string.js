a827d96aed190529ae6a33a6ed8b21915b7b919a
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:24:08 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/string.js
@@ -0,0 +1,14 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Types_et_grammaire
+ */
+
+var v1 = "Hello world";
+var v2 = "Hello \nworld";
+var v3 = "Unicode: \u00A9";
+
+print(v1);
+print(v1.length);
+
+print(v2);
+
+print(v3);
