8df04b5e44fde99407a7f6e0cefae5280ef9ac89
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-07-20 19:26:14 +0200

    Add a snippet (ECMAScript).

---
+++ javascript/ecmascript/math.js
@@ -0,0 +1,21 @@
+/*
+ * See: https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Nombres_et_dates#L%27objet_Math
+ */
+
+print("Math.PI", Math.PI)
+print("Math.sin(2)", Math.sin(2))
+
+print("Math.abs(-3)", Math.abs(-3))
+
+print("Math.pow(3, 2)", Math.pow(3, 2))
+print("Math.exp(1)", Math.exp(1))
+print("Math.sqrt(9)", Math.sqrt(9))
+
+print("Math.floor(3.14)", Math.floor(3.14))
+print("Math.ceil(3.14)", Math.ceil(3.14))
+
+print("Math.min(3.1, 4.2)", Math.min(3.1, 4.2))
+print("Math.max(3.1, 4.2)", Math.max(3.1, 4.2))
+
+print("Math.random()", Math.random())
+
