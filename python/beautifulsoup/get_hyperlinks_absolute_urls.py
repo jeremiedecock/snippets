02ee91b4d4b5e415124dec9fea2e3e70da8cc11d
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-09-18 01:31:34 +0200

    Add two snippets (python/beautifulsoup).

---
+++ python/beautifulsoup/get_hyperlinks_absolute_urls.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Jérémie DECOCK (http://www.jdhp.org)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Warning:
+# Urllib is part of the Python3 standard library but this is not the case for
+# urllib2 and urllib3 !
+# "urllib and urllib2 have little to do with each other. They were designed to
+# be independent and standalone, each solving a different scope of problems,
+# and urllib3 follows in a similar vein."
+
+# Required package (on Debian8):
+# - BeautifulSoup4: python3-bs4
+
+# Online documentation:
+# - BeautifulSoup4: http://www.crummy.com/software/BeautifulSoup/bs4/doc/
+# - Urllib:         https://docs.python.org/3/library/internet.html
+#                   https://docs.python.org/3/library/urllib.request.html
+
+import argparse
+from bs4 import BeautifulSoup
+import urllib.request
+from urllib.parse import urljoin
+
+def main():
+    """Main function"""
+
+    # PARSE OPTIONS ###########################################################
+
+    parser = argparse.ArgumentParser(description='A BeautifulSoup snippet.')
+    parser.add_argument("url", nargs=1, metavar="URL",
+                        help="The URL of the webpage to parse.")
+    args = parser.parse_args()
+
+    url = args.url[0]
+
+    # GET HTML ################################################################
+
+    html = urllib.request.urlopen(url).read()
+
+    # PARSE HTML ##############################################################
+
+    soup = BeautifulSoup(html)
+
+    for anchor in soup.find_all('a'):
+        relative_url = anchor.get('href')
+        absolute_url = urljoin(url, relative_url)
+        print(absolute_url)
+
+
+if __name__ == '__main__':
+    main()
+
