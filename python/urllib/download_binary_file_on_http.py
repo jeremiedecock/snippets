65e424661a79da5b512893025ba1b48196a368b7
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-09-18 00:53:23 +0200

    Add two snippets (Python/urllib).

---
+++ python/urllib/download_binary_file_on_http.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Jérémie DECOCK (http://www.jdhp.org)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Warning:
+# The urllib2 (from Python 2.x) module has been split across several modules in
+# Python 3 named "urllib.request" and "urllib.error".
+# Urllib (and thus urllib2) is part of the Python3 standard library but this is
+# not the case for urllib3 !
+# "urllib and urllib2 have little to do with each other. They were designed to
+# be independent and standalone, each solving a different scope of problems,
+# and urllib3 follows in a similar vein."
+
+# Online documentation:
+# - https://docs.python.org/3/library/urllib.request.html
+# - http://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
+# - http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
+
+import urllib.request
+
+def main():
+    """Main function"""
+
+    url = "https://www.google.fr/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+
+    # Download the file from "url" and save it locally under "out.png"
+    urllib.request.urlretrieve(url, "out.png")
+
+if __name__ == '__main__':
+    main()
+
---
+++ python/urllib/set_http_agent.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Jérémie DECOCK (http://www.jdhp.org)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Warning:
+# The urllib2 (from Python 2.x) module has been split across several modules in
+# Python 3 named "urllib.request" and "urllib.error".
+# Urllib (and thus urllib2) is part of the Python3 standard library but this is
+# not the case for urllib3 !
+# "urllib and urllib2 have little to do with each other. They were designed to
+# be independent and standalone, each solving a different scope of problems,
+# and urllib3 follows in a similar vein."
+
+# Online documentation:
+# - https://docs.python.org/3/library/urllib.request.html
+# - http://stackoverflow.com/questions/24226781/changing-user-agent-in-python-3-for-urrlib-urlopen
+# - http://stackoverflow.com/questions/802134/changing-user-agent-on-urllib2-urlopen
+
+import urllib.request
+
+def main():
+    """Main function"""
+
+    url = "http://myhttp.info/"
+
+    headers={
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0 Iceweasel/38.2.1',
+            'Accept encoding': 'gzip, deflate',
+            'Accept language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3'
+    }
+
+    request = urllib.request.Request(url, data=None, headers=headers)
+
+    # SIMPLY PRINT HTML #######################################################
+
+    response = urllib.request.urlopen(request)
+    html = response.read()
+    print(html)
+
+    # ALTERNATIVE: DOWNLOAD HTML ##############################################
+    # See http://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
+
+    import shutil
+
+    with urllib.request.urlopen(request) as response, open('out.html', 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+
+if __name__ == '__main__':
+    main()
+
