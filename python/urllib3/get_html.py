e8e54bedc826ce5c1ae4859686b445a558f19c7b
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-09-17 12:40:27 +0200

    Add a snippet (python/urllib3).

---
+++ python/urllib3/get_html.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Jérémie DECOCK (http://www.jdhp.org)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Required package (on Debian8):
+# - Urllib3:        python3-urllib3
+
+# Online documentation:
+# - Urllib3:        https://urllib3.readthedocs.org/en/latest/
+
+import argparse
+import urllib3
+
+def main():
+    """Main function"""
+
+    # PARSE OPTIONS ###########################################################
+
+    parser = argparse.ArgumentParser(description='A UrlLib snippet.')
+    parser.add_argument("url", nargs=1, metavar="URL",
+                        help="The URL of the webpage to parse.")
+    args = parser.parse_args()
+
+    url = args.url[0]
+    print("url:", url)
+
+    # GET HTML ################################################################
+
+    http = urllib3.PoolManager()
+    request = http.request('GET', url)
+    print("STATUS:", request.status)
+
+    html = request.data
+    print(html)
+
+
+if __name__ == '__main__':
+    main()
+
