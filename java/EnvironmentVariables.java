642b1258e2904ade2dec7cc545bbc4309f64fe5d
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-12-28 11:19:40 +0100

    Add a snippet (java).

---
+++ java/EnvironmentVariables.java
@@ -0,0 +1,21 @@
+/*
+ * This snippet show how to get an environment variable
+ * (e.g. the PATH environment variable here)
+ *
+ * For more information, read "Le guide de survie Java" (chap. 2)
+ * (by Timothy Fisher, ed. CampusPress)
+ *
+ * Usage:
+ * - compile: javac EnvironmentVariables.java
+ * - execute: java EnvironmentVariables
+ */
+
+public class EnvironmentVariables {
+    public static void main(String [] args) {
+
+        // Get the PATH environment variable
+        String envPath = System.getenv("PATH");
+
+        System.out.println(envPath);
+    }
+}
