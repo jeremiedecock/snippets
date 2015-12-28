fa6a172d3b191667b7e0ae9f214ab73ef80a3d8f
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-12-28 16:30:53 +0100

    Simplify a snippet.

--- java/swing/Hello.java
+++ java/swing/Hello.java
@@ -1,9 +1,7 @@
-package org.jdhp.snippets.swing;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class HelloSwing {
+public class Hello {
     public static void main(String args[]) {
         // Create a frame and set the title
         JFrame frame = new JFrame("Hello World!");
