84e6ef17b19da2e2bd4a96cad9fcf589c8afeb00
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-12-28 11:51:32 +0100

    Update a snippet.

--- java/Properties.java
+++ java/Properties.java
@@ -1,3 +1,5 @@
+import java.util.Enumeration;
+
 /*
  * This snippet show how to set and get properties.
  *
@@ -12,17 +14,23 @@
 public class Properties {
     public static void main(String [] args) {
 
-        String timezone;
-        
-        // Get the timezone property
-        timezone = System.getProperty("timezone");
-        System.out.println(timezone);
-
         // Set the timezone property
         System.setProperty("timezone", "EsternStandardTime");
         
         // Get the timezone property
-        timezone = System.getProperty("timezone");
+        String timezone = System.getProperty("timezone");
         System.out.println(timezone);
+        
+        // Get all properties
+        java.util.Properties properties = System.getProperties();
+        Enumeration propertyNames = properties.propertyNames();
+
+        String key;
+
+        while(propertyNames.hasMoreElements()) {
+            key = (String) propertyNames.nextElement();
+            System.out.println(key + " = " + properties.getProperty(key));
+        }
+
     }
 }
