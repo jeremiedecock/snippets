0d422a900105993b3bb75d432f7ea236547f9fcc
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-01-09 23:58:29 +0100

    Add a snippet (Groovy).

---
+++ groovy/types.groovy
@@ -0,0 +1,22 @@
+// Usage: groovy types.groovy
+
+// Boolean
+b = true;
+
+// Integer
+n = 1;
+
+// Float
+x = 3.14;
+
+// String
+s = "Hello";
+
+// List
+l = [1, 2, 3];
+
+// Map
+d = ["one":"un", "two":"deux", "three":"trois"];
+
+
+print b, n, x, s, l, d
