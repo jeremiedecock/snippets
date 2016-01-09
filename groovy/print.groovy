856d543ff0e45d52ab18c70c24fefc7ffb56540e
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-01-10 00:10:43 +0100

    Add a snippet (Groovy).

---
+++ groovy/print.groovy
@@ -0,0 +1,27 @@
+// Usage: groovy print.groovy
+
+// Print //////////////////////////////////////////////////
+
+System.out.print("Hello, World!")
+
+// or simply...
+print("Hello, World!")
+
+// or even...
+print "Hello, World!"
+
+// Println ////////////////////////////////////////////////
+
+System.out.println("Hello, World!")
+
+// or simply...
+println("Hello, World!")
+
+// or even...
+println "Hello, World!"
+
+// Value substitution /////////////////////////////////////
+
+var = 3;
+println "value = $var"
+
