17817741eacb07fe55536e4bfb22cf4b5ef005c6
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-01-10 00:02:22 +0100

    Fix a snippet.

--- groovy/types.groovy
+++ groovy/types.groovy
@@ -2,21 +2,24 @@
 
 // Boolean
 b = true;
+println b;
 
 // Integer
 n = 1;
+println n;
 
 // Float
 x = 3.14;
+println x;
 
 // String
 s = "Hello";
+println s;
 
 // List
 l = [1, 2, 3];
+println l;
 
 // Map
 d = ["one":"un", "two":"deux", "three":"trois"];
-
-
-print b, n, x, s, l, d
+println d;
