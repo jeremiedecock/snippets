88f054062a60abf0b318d12e112b14ed3c9c0481
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-01-10 00:40:08 +0100

    Add a snippet (Groovy).

---
+++ groovy/list.groovy
@@ -0,0 +1,51 @@
+// Usage: groovy list.groovy
+
+// Init ///////////////////////////////////////////////////
+
+l = [];
+println l;
+
+l = [1, 2, 3, 4, 5];
+println l;
+
+
+// Update /////////////////////////////////////////////////
+
+l.add(6)      // Add at the end
+println l;
+
+l.add(0, -1)  // Add at the begining
+println l;
+
+i = 0;
+l.remove(i);  // Remove the ith element
+
+l2 = l.clone()
+l2.clear()
+println l2
+
+
+// Size ///////////////////////////////////////////////////
+
+println l.size()
+
+
+// Get / Set //////////////////////////////////////////////
+
+l[3] = 40;      // Set
+
+println l[3];   // Get
+
+
+// Visit //////////////////////////////////////////////////
+
+// For loop
+for(elem in l) {
+    println "- $elem";
+}
+
+
+// Shuffle ////////////////////////////////////////////////
+
+java.util.Collections.shuffle(l);
+println l;
