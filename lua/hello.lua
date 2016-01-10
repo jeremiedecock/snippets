e13b448bad7db9d0a8fc457caff51861b907b2b1
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-01-10 10:35:00 +0100

    Add a snippet (Lua).

---
+++ lua/hello.lua
@@ -0,0 +1,14 @@
+#!/usr/bin/env lua5.3
+
+print("Hello World!")
+
+
+-- It can also be written as
+
+io.write('Hello World!\n')
+
+
+-- or, the example given on the Lua website
+
+io.write("Hello world, from ", _VERSION, "!\n")
+
