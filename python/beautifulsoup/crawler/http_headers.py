b61b6472f1a9d4d3877f2b7fdc8d7126ff737930
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-02-05 23:37:05 +0100

    Add a file.

---
+++ python/beautifulsoup/crawler/http_headers.py
@@ -0,0 +1,21 @@
+# Ce fichier doit être complété manuellement pour chaque site web cible.
+# Pour obtenir la liste des valeurs à inscrire à l'aide de Firefox:
+# - Consulter la page racine d'où lancer le téléchargement (s'identifier si besoin)
+# - Faire un clic droit sur la page et sélectionner "Examiner l'élément"
+# - Aller dans l'onglet "Réseau"
+# - Aller dans l'onglet "En-têtes"
+# - Copier-coller les valeurs inscrites dans "En-têtes de requête"
+
+# DO NOT COMMIT THIS FILE IF IT CONTAINS PERSONNAL SESSIONS IDs!!!
+
+HTTP_HEADERS = {
+    'User-Agent': '',
+    'Accept': '',
+    'Accept-Language': '',
+    'Accept-Encoding': '',
+    'Cookie': '',
+    'Connection': '',
+    'Cache-Control': ''
+}
+
+URL_DOMAIN = ''
