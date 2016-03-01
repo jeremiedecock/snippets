0b5018aceddc27c3d6e889932b35de10d74f01ed
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-03-01 16:33:08 +0100

    Add a widget.

---
+++ python/tkinter/python3/animation_label.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016 Jérémie DECOCK (http://www.jdhp.org)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# See also: http://effbot.org/tkinterbook/widget.htm
+
+import tkinter as tk
+
+counter = 0
+
+def main():
+    """Main function"""
+
+    root = tk.Tk()
+
+    label = tk.Label(root, text=str(counter))
+    label.pack(padx=30, pady=10)
+
+    def increment_counter():
+        # Do something...
+        global counter
+        counter += 1
+        label["text"] = str(counter)
+
+        # Reschedule event in 2 seconds
+        root.after(2000, increment_counter)
+
+    # Schedule event in 2 seconds
+    root.after(2000, increment_counter)
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
