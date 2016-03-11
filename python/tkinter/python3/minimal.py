e5e92fff857f367f1b0b6d79e6ac96b53e76b844
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-03-11 23:11:18 +0100

    Simplify a snippet.

--- python/tkinter/python3/minimal.py
+++ python/tkinter/python3/minimal.py
@@ -23,11 +23,5 @@
 
 import tkinter as tk
 
-def main():
-    """Main function"""
-
-    root = tk.Tk()
-    root.mainloop()
-
-if __name__ == '__main__':
-    main()
+root = tk.Tk()
+root.mainloop()
