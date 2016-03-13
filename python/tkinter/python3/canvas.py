1ff7fdf406b25ad77bedb09c7966d0dada43f82d
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-03-13 15:42:18 +0100

    Improve a snippet.

--- python/tkinter/python3/canvas.py
+++ python/tkinter/python3/canvas.py
@@ -30,18 +30,73 @@ root = tk.Tk()
 canvas = tk.Canvas(root, width=600, height=600, background="white")
 canvas.pack(padx=10, pady=10)
 
-# arrow: "none", "first", "last" or "both"
-canvas.create_line((0, 0, 200, 100), fill="red", width=4, dash=(8, 4), arrow="last", arrowshape=(16, 20, 6))
-canvas.create_line((0, 100, 200, 0), fill="black", width=2)
+# Lines #######################################################################
 
-canvas.create_rectangle((300, 0, 500, 100), outline="orange", fill="blue", width=2)
+# arrow: "none", "first", "last" or "both"
+canvas.create_line((10, 10, 90, 90),        # coordinates: (x1, y1, x2, y2)
+                   fill="red",
+                   width=4,
+                   dash=(8, 4),
+                   arrow="last",
+                   arrowshape=(16, 20, 6))
+
+canvas.create_line((110, 10, 190, 90),      # coordinates: (x1, y1, x2, y2)
+                   fill="black",
+                   width=2)
+
+coords1 = (210, 10, 250, 90, 290, 10)   # coordinates: (x1, y1, x2, y2, x3, y3)
+canvas.create_line(coords1,
+                   fill="black",
+                   width=5,
+                   joinstyle="round")
+canvas.create_line(coords1,
+                   fill="red",
+                   width=2,
+                   smooth=1)
+
+coords2 = (310, 10, 330, 90, 350, 10, 370, 90, 390, 10)   # coordinates: (x1, y1, x2, y2, x3, y3, ...)
+canvas.create_line(coords2,
+                   fill="black",
+                   width=2)
+canvas.create_line(coords2,
+                   fill="red",
+                   width=5,
+                   smooth=1)
+
+# Polygon #####################################################################
+
+coords3 = (210, 110, 250, 190, 290, 110)   # coordinates: (x1, y1, x2, y2, x3, y3)
+canvas.create_polygon(coords3,
+                      fill="red",
+                      outline="black",
+                      width=5,
+                      joinstyle="round")
+
+coords4 = (310, 110, 330, 190, 350, 150, 370, 190, 390, 110)   # coordinates: (x1, y1, x2, y2, x3, y3, ...)
+canvas.create_polygon(coords4,
+                      fill="red",
+                      activefill="blue",
+                      outline="black",
+                      width=2,
+                      joinstyle="round")
+
+# Rectangle ###################################################################
+
+canvas.create_rectangle((410, 110, 490, 190), # coordinates: (x1, y1, x2, y2)
+                        outline="orange",
+                        fill="blue",
+                        width=2)
+
+# Oval ########################################################################
 
 canvas.create_oval((0, 200, 200, 300), outline="purple", fill="yellow", width=2)
 
-# start: the start angle in degrees
-# extent: the size, relative to the start angle. Default is 90.0
-# style: "pieslice", "chord" or "arc"
-canvas.create_arc((300, 200, 500, 300), start=45, extent=225, style="arc", outline="brown", width=2)
+canvas.create_arc((300, 200, 500, 300),
+                  start=45,        # the start angle in degrees
+                  extent=225,      # the size, relative to the start angle (default is 90.0)
+                  style="arc",     # "pieslice", "chord" or "arc"
+                  outline="brown",
+                  width=2)
 
 canvas.create_polygon((0, 400, 50, 450, 100, 400, 150, 450), fill="black", width=2)
 
