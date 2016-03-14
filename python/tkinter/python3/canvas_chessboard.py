9ce61821a2d8fd6ce7ee7a894b1f320b1aae1fd8
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-03-14 20:13:12 +0100

    Add a snippet.

--- python/tkinter/python3/canvas_chessboard.py
+++ python/tkinter/python3/canvas_chessboard.py
@@ -38,15 +38,39 @@ canvas.pack(padx=10, pady=10)
 for row_index in range(SQUARE_NUM):
     for col_index in range(SQUARE_NUM):
         color = "white" if (row_index + col_index) % 2 == 0 else "black"
+        tags = ("square", "{}x{}".format(col_index, row_index))
+
         canvas.create_rectangle(SQUARE_SIZE * col_index,       # x1
                                 SQUARE_SIZE * row_index,       # y1
                                 SQUARE_SIZE * (col_index + 1), # x2
                                 SQUARE_SIZE * (row_index + 1), # y2
+                                tag=tags,
                                 fill=color,
                                 activefill="firebrick3",
                                 #activeoutline="firebrick1", # does not work if "width=0"
                                 #activewidth=4,              # does not work if "width=0"
                                 width=0)
+
+# Bind items ##################################################################
+
+def callback(event):               # event is a tkinter.Event object
+    id_tuple = canvas.find_withtag("current")  # get the item which is under the mouse cursor
+
+    if len(id_tuple) > 0:
+        item_id = id_tuple[0]
+
+        #print(canvas.gettags(item_id))
+        item_tag1, item_tag2, item_tag3 = canvas.gettags(item_id)
+
+        print("You have clicked on the square {} (item #{})".format(item_tag2, item_id))
+        print([int(coord) for coord in item_tag2.split("x")])
+    else:
+        raise Exception("Unexpected error")
+
+canvas.tag_bind("square",          # a tag string or an item id
+                "<Button-1>",      # the event descriptor
+                callback,          # the callback function
+                add="+")           # "+" to add this binding to the previous one(s) (i.e. keep the previous binding(s)) or None to replace it or them
 
 # Main loop ###################################################################
 
