523d3c2cc3f475b64fc099924c793444a84cad69
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-04-08 18:11:08 +0200

    Add a snippet.

---
+++ python/os/path_basename.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+file_path = '/path/to/somefile.ext'
+
+basename = os.path.basename(file_path)
+
+print("File path:", file_path)
+print("Basename:", basename)
