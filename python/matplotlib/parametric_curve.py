8a05d1d2b9d3ef94866eaba8c704a426e9c968c1
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-05-12 11:19:14 +0200

    Fix some mistakes.

--- python/matplotlib/parametric_curve.py
+++ python/matplotlib/parametric_curve.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 
 # Build datas ###############
 
-t = np.linspace(0, np.pi, 100)
+t = np.linspace(0, 2. * np.pi, 200)
 
-x = 0.7 * np.sin(t / 2.) * np.sin(3. / 2. * t)
-y = 0.7 * np.cos(t) * np.sin(2. * t)
+x = 0.7 * np.sin(t) * np.sin(3. * t)
+y = 0.7 * np.cos(t) * np.sin(3. * t)
 
 # Plot data #################
 
@@ -17,6 +17,9 @@ fig = plt.figure(figsize=(8.0, 8.0))
 ax = fig.add_subplot(111)
 
 ax.plot(x, y)
+
+ax.set_xlim([-0.8, 0.8])
+ax.set_ylim([-0.8, 0.8])
 
 # Save file and plot ########
 
