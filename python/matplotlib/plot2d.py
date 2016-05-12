c1121bb3dc62d3af1d1f7ec365411df4bf174273
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-05-12 10:49:51 +0200

    Misc updates.

--- python/matplotlib/plot2d.py
+++ python/matplotlib/plot2d.py
@@ -5,28 +5,32 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-def main():
-    x_vec = np.arange(-10, 10, 0.01)
-    y_vec = np.sin(2 * 2 * np.pi * x_vec) * 1/np.sqrt(2*np.pi) * np.exp(-(x_vec**2)/2)
+# Build datas ###############
 
-    fig = plt.figure(figsize=(16.0, 10.0))
-    ax = fig.add_subplot(111)
-    ax.plot(x_vec, y_vec, "-", label="Test")
+x = np.arange(-10, 10, 0.01)
+y = np.sin(2. * 2. * np.pi * x) * 1. / np.sqrt(2. * np.pi) * np.exp(-(x**2.)/2.)
 
-    # TITLE AND LABELS
-    ax.set_title(r"Test", fontsize=20)
-    ax.set_xlabel(r"$x$", fontsize=32)
-    ax.set_ylabel(r"$f(x)$", fontsize=32)
+# Plot data #################
 
-    # LEGEND
-    ax.legend(loc='lower right', fontsize=20)
+fig = plt.figure(figsize=(8.0, 8.0))
+ax = fig.add_subplot(111)
 
-    # SAVE FILES ######################
-    plt.savefig("plot2d.png")
+ax.plot(x, y, "-", label="Test")
 
-    # PLOT ############################
-    plt.show()
+# Set title and labels ######
 
-if __name__ == "__main__":
-    main()
+ax.set_title(r"Test", fontsize=20)
+ax.set_xlabel(r"$x$", fontsize=32)
+ax.set_ylabel(r"$f(x)$", fontsize=32)
 
+# Set legend ################
+
+ax.legend(loc='lower right', fontsize=20)
+
+# Save file #################
+
+plt.savefig("plot2d.png")
+
+# Plot ######################
+
+plt.show()
