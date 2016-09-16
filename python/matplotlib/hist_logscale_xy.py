4fb49f1decc7dcf3a3dd09b11f8c7f85bc05620b
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-09-16 15:48:15 +0200

    Add a snippet.

---
+++ python/matplotlib/hist_logscale_xy.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# See:
+# - 
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# SETUP #######################################################################
+
+# histtype : [‘bar’ | ‘barstacked’ | ‘step’ | ‘stepfilled’]
+HIST_TYPE='bar'
+ALPHA=0.5
+
+# MAKE DATA ###################################################################
+
+data = np.random.exponential(size=1000000)
+#data = np.abs(np.random.normal(size=1000000) * 10000.)
+#data = np.random.chisquare(10, size=1000000)
+
+# INIT FIGURE #################################################################
+
+fig = plt.figure(figsize=(8.0, 6.0))
+
+# AX1 #########################################################################
+
+ax1 = fig.add_subplot(211)
+
+res_tuple = ax1.hist(data,
+                     bins=50,
+                     histtype=HIST_TYPE,
+                     alpha=ALPHA)
+
+ax1.set_title("Normal scale")
+ax1.set_xlabel("Value")
+ax1.set_ylabel("Count")
+
+# AX2 #########################################################################
+
+ax2 = fig.add_subplot(212)
+
+min = np.log10(data.min())
+max = np.log10(data.max())
+bins = np.logspace(min, max, 50)    # <- create a range from 10**min to 10**max
+
+print(bins)
+
+res_tuple = ax2.hist(data,
+                     log=True,                # <- Activate log scale on Y axis
+                     bins=bins,
+                     histtype=HIST_TYPE,
+                     alpha=ALPHA)
+
+ax2.set_xscale("log")               # <- Activate log scale on Y axis
+
+ax2.set_title("Log scale")
+ax2.set_xlabel("Value")
+ax2.set_ylabel("Count")
+
+# SHOW AND SAVE FILE ##########################################################
+
+plt.tight_layout()
+
+plt.savefig("hist_logscale_xy.png")
+plt.show()
