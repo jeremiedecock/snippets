48eb8f30b9bf506b6e5cebb404b95078a3240674
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-11-23 17:36:23 +0100

    Add a snippet.

---
+++ python/numpy/multiple_type_array.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# See http://stackoverflow.com/questions/16621351/how-to-use-python-numpy-savetxt-to-write-strings-and-float-number-to-an-ascii-fi
+
+import numpy as np
+
+foo = np.array([
+                   (1, 'Sirius', -1.45, 'A1V'),
+                   (2, 'Canopus', -0.73, 'F0Ib'),
+                   (3, 'Rigil Kent', -0.1, 'G2V')
+               ],
+               dtype=[
+                   ('order', 'int16'),
+                   ('name', 'a20'),
+                   ('mag', 'float32'),
+                   ('Sp', 'a10')
+               ])
+
+print(foo)
+print(foo.dtype)
