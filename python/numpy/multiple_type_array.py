dbe8231cdce12ed80602a172528aa84c7e62d0b3
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-11-23 17:46:45 +0100

    Misc updates.

--- python/numpy/multiple_type_array.py
+++ python/numpy/multiple_type_array.py
@@ -5,6 +5,20 @@
 
 import numpy as np
 
+# METH 1
+
+foo = np.array([
+                   (1, 'Sirius', -1.45, 'A1V'),
+                   (2, 'Canopus', -0.73, 'F0Ib'),
+                   (3, 'Rigil Kent', -0.1, 'G2V')
+               ],
+               dtype='int16, a20, float32, a10')
+
+print(foo)
+print(foo.dtype)
+
+# METH 2
+
 foo = np.array([
                    (1, 'Sirius', -1.45, 'A1V'),
                    (2, 'Canopus', -0.73, 'F0Ib'),
@@ -16,6 +30,21 @@ foo = np.array([
                    ('mag', 'float32'),
                    ('Sp', 'a10')
                ])
+
+print(foo)
+print(foo.dtype)
+
+# METH 3
+
+foo = np.array([
+                   (1, 'Sirius', -1.45, 'A1V'),
+                   (2, 'Canopus', -0.73, 'F0Ib'),
+                   (3, 'Rigil Kent', -0.1, 'G2V')
+               ],
+               dtype={
+                   'names': ['order', 'name', 'mag', 'Sp'],
+                   'formats': ['int16', 'a20', 'float32', 'a10']
+               })
 
 print(foo)
 print(foo.dtype)
