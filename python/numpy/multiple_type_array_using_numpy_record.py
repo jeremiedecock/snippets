efbe59e900fb95b74834ab2e277e01782449a4cd
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-11-23 17:36:45 +0100

    Misc update.

--- python/numpy/multiple_type_array_using_numpy_record.py
+++ python/numpy/multiple_type_array_using_numpy_record.py
@@ -5,9 +5,9 @@
 import numpy as np
 
 foo = np.rec.array([
-                    (1, 'Sirius', -1.45, 'A1V'),
-                    (2, 'Canopus', -0.73, 'F0Ib'),
-                    (3, 'Rigil Kent', -0.1, 'G2V')
+                       (1, 'Sirius', -1.45, 'A1V'),
+                       (2, 'Canopus', -0.73, 'F0Ib'),
+                       (3, 'Rigil Kent', -0.1, 'G2V')
                    ],
                    formats='int16, a20, float32, a10',
                    names='order, name, mag, Sp')
