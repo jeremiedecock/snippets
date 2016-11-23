0635109abeabc94b6c908379064947f154cab96f
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-11-23 14:48:34 +0100

    Improve a snippet.

--- python/numpy/savetxt_with_header.py
+++ python/numpy/savetxt_with_header.py
@@ -2,11 +2,18 @@
 
 import numpy as np
 
-a = np.random.rand(10, 4)
+def save_np_array(output_file_path, data_array, header_list):
 
-fd = open("test.dat", "w")
-fd.write("# This is a test\n")
-fd.write("# with a header\n")
-np.savetxt(fd, a)
-fd.close()
+    np.savetxt(output_file_path,
+               data_array,
+               #fmt="%10.5f",
+               #delimiter=" ",
+               header="; ".join(header_list),
+               #comments="# "                 # String that will be prepended to the ``header`` and ``footer`` strings, to mark them as comments. Default: '# '.
+               )
 
+
+data = np.random.rand(10, 4)
+header = ["rand 1", "rand 2", "rand 3", "rand 4"]
+
+save_np_array("test.dat", data, header)
