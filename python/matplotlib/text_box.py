8648933314795f597e0b278a2659f52b3dbf32a4
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-01-18 11:42:14 +0100

    Add a snippet.

---
+++ python/matplotlib/text_box.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(10, 6))
+
+data = np.random.binomial(n=100, p=0.25, size=10000)
+ax1.hist(data)
+
+# Info box
+ax1.text(0.05, 0.92,
+        "Hello world!\nThis is a text box demo",
+        verticalalignment = 'top',
+        horizontalalignment = 'left',
+        transform = ax1.transAxes,
+        bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
+
+# Info box
+ax1.text(0.95, 0.92,
+        "Another text box",
+        verticalalignment = 'top',
+        horizontalalignment = 'right',
+        transform = ax1.transAxes,
+        bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
+
+# Save file and plot ########
+
+plt.savefig("text_box.png", bbox_inches='tight')
+plt.show()
