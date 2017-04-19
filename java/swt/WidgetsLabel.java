9c5d58062df95fefb336dc9280e7754a56fd5941
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-04-19 21:58:52 +0200

    Add a snippet.

---
+++ java/swt/WidgetsLabel.java
@@ -0,0 +1,39 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class WidgetsLabel {
+
+    public static void main(String [] args) {
+        Display display = new Display();
+
+        // Make the window
+        Shell shell = new Shell(display);
+        shell.setLayout(new GridLayout(1, false));
+
+        shell.setText("Snippet SWT");
+        shell.setMinimumSize(400, 350);
+        shell.setSize(640, 480);
+
+        // Add a widget
+        Label label = new Label(shell, SWT.NONE);
+        label.setLayoutData(new GridData(GridData.FILL_BOTH));
+        label.setText("Hello World");
+        label.pack();
+
+        // Render the window
+        shell.pack();
+        shell.open();
+
+        while(!shell.isDisposed()) {
+            if(!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+
+        display.dispose();
+    }
+}
