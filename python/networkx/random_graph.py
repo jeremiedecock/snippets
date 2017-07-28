a4565e837a542e9363be27e76ceca7320d51f59f
Jeremie DECOCK <jd.jdhp@gmail.com>
2017-07-28 13:18:11 +0200

    Add snippets.

---
+++ python/networkx/random_graph.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017 Jérémie DECOCK (http://www.jdhp.org)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+g = nx.gnm_random_graph(10, 20)
+
+print(list(g.nodes()))
+print(list(g.edges()))
+
+pos = nx.spring_layout(g)
+
+nx.draw_networkx_nodes(g, pos, node_size=20)
+nx.draw_networkx_edges(g, pos, alpha=0.4)
+nx.draw_networkx_labels(g, pos, alpha=0.4)
+
+plt.savefig("random_graph.png")
+plt.show()
