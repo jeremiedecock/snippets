e35ee181ccd39339398c66c1b6644a88d0e37c79
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-01-16 17:05:43 +0100

    Add a snippet.

--- python/matplotlib/text_box.py
+++ python/matplotlib/text_box.py
@@ -1,37 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2016,2017 Jérémie DECOCK (http://www.jdhp.org)
-
 """
-Add a text box
+Plot an histogram with a text stats summary
+
+See:
+
+- https://stackoverflow.com/questions/8482588/putting-text-in-top-left-corner-of-matplotlib-plot
 """
 
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(10, 6))
+# MAKE DATA ###################################################################
 
-data = np.random.binomial(n=100, p=0.25, size=10000)
-ax1.hist(data)
+data = np.random.normal(size=1000)
 
-# Info box
-ax1.text(0.05, 0.92,
-         "Hello world!\nThis is a text box demo",
-         verticalalignment = 'top',
-         horizontalalignment = 'left',
-         transform = ax1.transAxes,
-         bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
+# AX1 #########################################################################
 
-# Info box
-ax1.text(0.95, 0.92,
-         "Another text box",
-         verticalalignment = 'top',
-         horizontalalignment = 'right',
-         transform = ax1.transAxes,
-         bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
+fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(7, 7))
+    
+ax.hist(data,
+         bins=50,
+         #range=(-10, 2500),
+         log=False)
 
-# Save file and plot ########
+ax.set_ylabel("Counts", fontsize=18)
+ax.set_title("Gaussian", fontsize=20)
+    
+sig_info = "Samples: {}\nMin: {:0.2f}\nMax: {:0.2f}\nMean: {:0.2f}\nSTD: {:0.2f}".format(len(data),
+                                                                                         data.min(),
+                                                                                         data.max(),
+                                                                                         data.mean(),
+                                                                                         data.std())
 
-plt.savefig("text_box.png", bbox_inches='tight')
+ax.text(0.95, 0.95, sig_info,
+         horizontalalignment='right',
+         verticalalignment='top',
+         fontsize=18,
+         transform = ax.transAxes)
+print(sig_info)
+
+# SHOW AND SAVE FILE ##########################################################
+
+plt.tight_layout()
+
+plt.savefig("text_box.png")
 plt.show()
