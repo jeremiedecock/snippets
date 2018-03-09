e84e449ea8733cd1c0a6b65d80706de35d9c4b0e
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-03-09 13:15:18 +0100

    Misc update.

--- python/matplotlib/text_box.py
+++ python/matplotlib/text_box.py
@@ -12,6 +12,24 @@ See:
 import numpy as np
 import matplotlib.pyplot as plt
 
+###############################################################################
+
+def add_stat_box(ax, samples):
+    sig_info = "Samples: {}\nMin: {:0.2f}\nMax: {:0.2f}\nMean: {:0.2f}\nSTD: {:0.2f}".format(len(samples),
+                                                                                             samples.min(),
+                                                                                             samples.max(),
+                                                                                             samples.mean(),
+                                                                                             samples.std())
+
+    # https://stackoverflow.com/questions/8482588/putting-text-in-top-left-corner-of-matplotlib-plot
+    ax.text(0.95, 0.95, sig_info,
+            horizontalalignment='right',
+            verticalalignment='top',
+            fontsize=18,
+            transform = ax.transAxes)
+
+    return ax
+
 # MAKE DATA ###################################################################
 
 data = np.random.normal(size=1000)
@@ -21,25 +39,14 @@ data = np.random.normal(size=1000)
 fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(7, 7))
     
 ax.hist(data,
-         bins=50,
-         #range=(-10, 2500),
-         log=False)
+        bins=50,
+        #range=(-10, 2500),
+        log=False)
 
 ax.set_ylabel("Counts", fontsize=18)
 ax.set_title("Gaussian", fontsize=20)
     
-sig_info = "Samples: {}\nMin: {:0.2f}\nMax: {:0.2f}\nMean: {:0.2f}\nSTD: {:0.2f}".format(len(data),
-                                                                                         data.min(),
-                                                                                         data.max(),
-                                                                                         data.mean(),
-                                                                                         data.std())
-
-ax.text(0.95, 0.95, sig_info,
-         horizontalalignment='right',
-         verticalalignment='top',
-         fontsize=18,
-         transform = ax.transAxes)
-print(sig_info)
+add_stat_box(ax, data)
 
 # SHOW AND SAVE FILE ##########################################################
 
