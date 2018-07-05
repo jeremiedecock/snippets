718a349eb14e004a6ff83502ae91ed5285eec164
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-07-05 16:16:54 +0200

    Convert to Python3.

--- python/pickle/test.py
+++ python/pickle/test.py
@@ -1,16 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 import pickle
 
+
+# Original object ###########
+
 obj = {'hello':1, 'world':2}
 
-print obj
+print(obj)
+
+
+# Serialized object #########
 
 str_obj = pickle.dumps(obj)
 
-print str_obj
+print(str_obj)
+
+
+# Unserialized object #######
 
 obj = pickle.loads(str_obj)
 
-print obj
+print(obj)
