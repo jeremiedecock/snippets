2c900f05c0e34ff5637a8cb8b641440115e27e8b
Jérémie Decock <jd.jdhp@gmail.com>
2018-09-19 23:18:37 +0200

    Add a snippet.

---
+++ python/pyqt/pyqt5/widget_QPainter_draw_ellipse.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# See https://www.youtube.com/watch?v=U1Nitj0iOgU
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QVBoxLayout
+from PyQt5.QtGui import QPainter, QBrush, QColor, QPen
+from PyQt5.QtCore import Qt
+
+
+class MyPaintWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        # Set window background color
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(self.backgroundRole(), Qt.white)
+
+        self.setPalette(palette)
+
+    def paintEvent(self, event):
+        qp = QPainter(self)
+
+        qp.setPen(QPen(Qt.black, 5, Qt.SolidLine))
+        qp.setBrush(QBrush(Qt.red, Qt.SolidPattern))
+        qp.setRenderHint(QPainter.Antialiasing)           # <- Set anti-aliasing  See https://wiki.python.org/moin/PyQt/Painting%20and%20clipping%20demonstration
+
+        qp.drawEllipse(100, 15, 400, 200)
+
+        qp.setBrush(QBrush(Qt.red, Qt.DiagCrossPattern))
+
+        qp.drawEllipse(600, 15, 200, 200)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    widget = MyPaintWidget()
+    widget.show()
+
+    # The mainloop of the application. The event handling starts from this point.
+    # The exec_() method has an underscore. It is because the exec is a Python keyword. And thus, exec_() was used instead.
+    exit_code = app.exec_()
+
+    # The sys.exit() method ensures a clean exit.
+    # The environment will be informed, how the application ended.
+    sys.exit(exit_code)
