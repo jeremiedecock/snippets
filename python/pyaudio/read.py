3b00930f9c6e6552bef5b5939916a1b8e737287a
Jérémie Decock <jd.jdhp@gmail.com>
2018-10-07 20:44:34 +0200

    Add a snippet.

---
+++ python/pyaudio/read.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# See http://people.csail.mit.edu/hubert/pyaudio/docs/#example-blocking-mode-audio-i-o
+
+import pyaudio
+import wave
+
+CHUNK = 1024
+
+wf = wave.open("test.wav", 'rb')
+
+print(wf.getnchannels())
+print(wf.getframerate())
+
+p = pyaudio.PyAudio()
+print(p.get_device_count())
+
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True,
+                output_device_index=3)
+
+# read data
+data = wf.readframes(CHUNK)
+
+while len(data) > 0:
+    stream.write(data)
+    data = wf.readframes(CHUNK)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
