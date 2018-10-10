6a68fb0cac701b0fc03599e0a0ff25b385f728e0
Jérémie Decock <jd.jdhp@gmail.com>
2018-10-10 11:53:51 +0200

    Add two snippets.

---
+++ python/pyqt/pyqt5/widget_QDoubleSpinBox.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# See http://doc.qt.io/qt-5/qdoublespinbox.html#details
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QDoubleSpinBox, QPushButton, QVBoxLayout
+
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        # Make widgets #################
+
+        self.spinbox = QDoubleSpinBox()
+        self.spinbox.setMinimum(-15)
+        self.spinbox.setMaximum(15)
+
+        self.btn = QPushButton("Print")
+
+        # Set button slot ##############
+
+        self.btn.clicked.connect(self.printText)
+
+        # Set the layout ###############
+
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(self.spinbox)
+        vbox.addWidget(self.btn)
+
+        self.setLayout(vbox)
+
+    def printText(self):
+        print(self.spinbox.value())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.show()
+
+    # The mainloop of the application. The event handling starts from this point.
+    # The exec_() method has an underscore. It is because the exec is a Python keyword. And thus, exec_() was used instead.
+    exit_code = app.exec_()
+
+    # The sys.exit() method ensures a clean exit.
+    # The environment will be informed, how the application ended.
+    sys.exit(exit_code)
