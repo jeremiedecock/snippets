99becdaff6f665f05b30ab3ddbbf8c71ea842c1f
Jérémie Decock <jd.jdhp@gmail.com>
2018-11-19 22:12:41 +0100

    Misc updates.

--- java/Arrays.java
+++ java/Arrays.java
@@ -7,17 +7,30 @@
 public class Arrays {
     public static void main(String[] args) {
 
-		//int[] array = new int[3];   // OK
-		//int array[] = new int[3];   // OK
+        //int[] t = new int[3];   // OK
+        //int t[] = new int[3];   // OK
 
-        //int[] array = new int[3]{1, 2, 3};  // Error
-        //int[] array = new int[]{1, 2, 3};   // OK
-        //int array[] = new int[]{1, 2, 3};   // OK
-        //int array[] = {1, 2, 3};   // OK
-        int[] array = {1, 2, 3};   // OK
-		
-		for(int elem : array)
-			System.out.println(elem);
+        //int[] t = new int[3]{1, 2, 3};  // Error
+        //int[] t = new int[]{1, 2, 3};   // OK
+        //int t[] = new int[]{1, 2, 3};   // OK
+        //int t[] = {1, 2, 3};            // OK
+        int[] t = {1, 2, 3};              // OK
 
+        System.out.println(t.length);
+        System.out.println();
+
+        for(int elem : t)
+            System.out.println(elem);
+
+        /*
+        int t[][] = {{1, 2, 3}, {4, 5, 6}};                       // OK
+        //int[][] t = {{1, 2, 3}, {4, 5, 6}};                     // OK
+        //int t[][] = new int[][]{{1, 2, 3}, {4, 5, 6}};          // OK
+        //int[][] t = new int[][]{{1, 2, 3}, {4, 5, 6}};          // OK
+
+        for(int[] row : t)
+            for(int elem : row)
+                System.out.println(elem);
+        */
     }
 }
