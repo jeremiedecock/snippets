8da0f6b108695d814ad05932698b43cce905ff17
Jeremie DECOCK <jd.jdhp@gmail.com>
2019-02-14 17:38:36 +0100

    Complete a snippet.

--- go/variables.go
+++ go/variables.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 
     var s1 string = "hello"
-    s2 := "hello"               // shorthand syntax for declaring and initializing a variable
+    var s2 = "hello"            // infer the type of initialized variables
+    s3 := "hello"               // shorthand syntax for declaring and initializing a variable
 
     fmt.Println(s1)
     fmt.Println(s2)
+    fmt.Println(s3)
 
 }
