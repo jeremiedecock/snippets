950d2542e4bcc8868397bafa850d5f83957820f5
Jeremie DECOCK <jd.jdhp@gmail.com>
2019-02-14 17:34:52 +0100

    Add two snippets.

---
+++ go/values.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// https://gobyexample.com/values
+
+func main() {
+    fmt.Println("go" + "lang")
+
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
---
+++ go/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var s1 string = "hello"
+    s2 := "hello"               // shorthand syntax for declaring and initializing a variable
+
+    fmt.Println(s1)
+    fmt.Println(s2)
+
+}
