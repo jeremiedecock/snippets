d5422cbbf3b95f8f869fed1198c0c613cfbc6b80
Jeremie DECOCK <jd.jdhp@gmail.com>
2019-02-14 17:50:56 +0100

    Misc update.

--- go/constant.go
+++ go/constant.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 
     const s1 string = "hello"
-    const s2 = "hello"            // infer the type of initialized variables
+    const s2 = "hello"            // infer the type of initialized constant
 
     //s2 = "hi"
 
