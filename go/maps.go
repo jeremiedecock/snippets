5ac719beca3bc2d34a2eee29238aaf6d2f2cdf57
Jeremie DECOCK <jd.jdhp@gmail.com>
2019-02-15 17:27:45 +0100

    Misc update.

--- go/maps.go
+++ go/maps.go
@@ -19,8 +19,12 @@ func main() {
     delete(m, "two")
     fmt.Println("map:", m)
 
-    _, prs := m["two"]
-    fmt.Println("prs:", prs)
+    _, prs := m["three"]
+    if prs == true {
+        fmt.Println("three is present")
+    } else {
+        fmt.Println("three is absent")
+    }
 
     m2 := map[string]int{"one": 1, "two": 2}
     fmt.Println("map:", m2)
