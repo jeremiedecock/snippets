ae214b799587187276ee8d132edfba98a3f1bfc3
Jeremie DECOCK <jd.jdhp@gmail.com>
2020-12-06 19:13:13 +0100

    Misc up.

--- javascript/ecmascript/enumerate.js
+++ javascript/ecmascript/enumerate.js
@@ -3,16 +3,16 @@ const foo = ['A', 'B', 'C'];
 for(const [index, value] of foo.entries()) 
     console.log(index, value);
 
-// Alternative method
 
-console.log()
 
-for (const [index, value] of foo.entries()) {
-    console.log(index, value);
-}
 
 // Alternative method
 
 console.log()
 
 foo.map((value, index) => console.log(index, value));
+
+
+
+
+bar = foo.map((value, index) => index + ":" + value);
