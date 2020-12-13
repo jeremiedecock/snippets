f15bbb9a95f17d5b4dd8804e16f39e49863d9c87
Jeremie DECOCK <jd.jdhp@gmail.com>
2020-12-13 21:45:50 +0100

    Complete a snippet.

--- python/requests/hello_post.py
+++ python/requests/hello_post.py
@@ -5,6 +5,14 @@ import requests
 
 payload = {'key1': 'value1', 'key2': 'value2'}
 
+# RAW DATA
+
 r = requests.post("https://httpbin.org/post", data=payload)
+
+print(r.text)
+
+# JSON DATA
+
+r = requests.post("https://httpbin.org/post", json=payload)
 
 print(r.text)
