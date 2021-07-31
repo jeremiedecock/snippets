39e513fbaddba37c9817f11f885f1eddc93c3dfa
Jeremie DECOCK <jd.jdhp@gmail.com>
2021-07-31 21:15:12 +0200

    Add an 'hello' snippet + run script (for Python venv).

--- python/pyside/pyside6/hello.py
+++ python/pyside/pyside6/hello.py
@@ -2,28 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6 import QtCore, QtWidgets
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
-
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
 
 
 if __name__ == "__main__":
---
+++ python/pyside/pyside6/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#python3 --version
+
+#conda init bash
+#conda deactivate
+source env/bin/activate
+
+#python3 --version
+
+python3 $1
