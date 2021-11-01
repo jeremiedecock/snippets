1d9dccb54c31a427ad307e59edfe603899f161f4
Jeremie DECOCK <jd.jdhp@gmail.com>
2021-11-01 19:15:38 +0100

    Add snippets.

--- python/pyside/pyside6/widget_QSqlTableModel_sqlite_from_file_with_sort_and_filter.py
+++ python/pyside/pyside6/widget_QSqlTableModel_sqlite_from_file_with_sort_and_filter.py
@@ -7,8 +7,8 @@ import sys
 import sqlite3
 from PySide6 import QtCore, QtWidgets
 
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QTableView
+from PySide6.QtCore import Qt, QSortFilterProxyModel
+from PySide6.QtWidgets import QApplication, QWidget, QTableView, QLineEdit, QVBoxLayout
 from PySide6.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
 
 
@@ -18,7 +18,7 @@ con = sqlite3.connect("employee.db")
 cur = con.cursor()
 
 cur.execute("DROP TABLE employee")
-cur.execute("CREATE TABLE employee (first_name TEXT, last_name TEXT)")
+cur.execute("CREATE TABLE employee (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, last_name TEXT)")
 
 params_list = [
     ("Jean", "Dupont"),
@@ -41,15 +41,54 @@ assert db.open()
 #################################################
 
 app = QApplication(sys.argv)
+window = QWidget()
 
+# Make widgets ##############
+
+edit = QLineEdit()
 table_view = QTableView()
+
+edit.setPlaceholderText("Filter text (on col. 1)")
+
+# Set the layout ############
+
+vbox = QVBoxLayout()
+
+vbox.addWidget(edit)
+vbox.addWidget(table_view)
+
+window.setLayout(vbox)
+
+#############################
 
 model = QSqlTableModel()
 model.setTable("employee")
+#model.setEditStrategy(QSqlTableModel.OnManualSubmit)
 model.select()
+model.setHeaderData(0, Qt.Horizontal, "First Name")
+model.setHeaderData(1, Qt.Horizontal, "Last Name")
 
 table_view.setModel(model)
-table_view.show()
+table_view.setSortingEnabled(True)
+
+table_view.hideColumn(0) # don't show the ID
+
+# Set LineEdit slot #########################
+
+def foo():
+    filter_str = edit.text()
+    if filter_str == '':
+        model.setFilter("")
+    else:
+        model.setFilter("first_name LIKE '%{}%'".format(filter_str))
+    print(filter_str)
+
+
+edit.textChanged.connect(foo)
+
+#############################
+
+window.show()
 
 # The mainloop of the application. The event handling starts from this point.
 exit_code = app.exec()
