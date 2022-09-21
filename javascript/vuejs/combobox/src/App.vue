292260708a09045114e5e633912147f1f93ab0a5
Jeremie DECOCK <jd.jdhp@gmail.com>
2022-09-21 22:48:58 +0200

    Add snippets.

---
+++ javascript/vuejs/combobox/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      foo: 'bonjour'
+    }
+  }
+}
+</script>
+
+
+<template>
+  <select v-model="foo">
+    <option value="bonjour">Bonjour</option>
+    <option value="hello">Hello</option>
+  </select>
+
+  <p>{{ foo }}</p>
+</template>
+
+
+<style>
+</style>
---
+++ javascript/vuejs/slider/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      foo: 100
+    }
+  }
+}
+</script>
+
+
+<template>
+  <!-- controls -->
+  <div>
+    <input type="range" v-model="foo" min="0" max="100">
+    <span>{{foo}}</span>
+  </div>
+</template>
+
+
+<style>
+input {
+  display: block;
+  margin-bottom: 1em;
+}
+</style>
