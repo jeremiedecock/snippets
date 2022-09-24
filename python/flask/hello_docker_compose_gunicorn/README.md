9cc60299fe19cc85f65ac983a0f90784f3219db1
Jeremie DECOCK <jd.jdhp@gmail.com>
2022-09-25 00:25:48 +0200

    Add a snippet.

---
+++ python/flask/hello_docker_compose_gunicorn/README.md
@@ -0,0 +1,9 @@
+# Hello
+
+### Build the Flask app and run Nginx
+
+```sh
+docker-compose up
+```
+
+Then open a web browser and load page http://localhost:8080/
---
+++ python/flask/hello_docker_compose_gunicorn/wsgi/hello/core.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# C.f. https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app = Flask(__name__)
+
+# C.f. https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
