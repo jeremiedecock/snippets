96837795b23f2cbca7ea74cf6d2cd37bc39d4d24
Jeremie DECOCK <jd.jdhp@gmail.com>
2022-09-25 23:33:07 +0200

    Fix CORS error.

--- python/flask/hello_docker_compose_gunicorn_nginx_vuejs_prod/flask/hello/core.py
+++ python/flask/hello_docker_compose_gunicorn_nginx_vuejs_prod/flask/hello/core.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 # C.f. https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+CORS(app)
 
 # C.f. https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
