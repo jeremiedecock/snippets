60e37cefe9259db8fe166c82fadbedcb6d2bce4f
Jeremie DECOCK <jd.jdhp@gmail.com>
2022-09-25 23:42:43 +0200

    Restrict a bit the cross-origin policy.

--- python/flask/hello_docker_compose_gunicorn_nginx_vuejs_prod/flask/hello/core.py
+++ python/flask/hello_docker_compose_gunicorn_nginx_vuejs_prod/flask/hello/core.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import cross_origin
 
 # C.f. https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-CORS(app)
 
 # C.f. https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 @app.route("/")
+@cross_origin(origins=['http://127.0.0.1'])
 def hello_world():
     return {"message": "Hello from Flask!"}
