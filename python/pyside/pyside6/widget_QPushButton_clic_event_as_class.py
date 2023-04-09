bd1fd2396411c908ea639e08cbf5791c7d1cec64
Jeremie DECOCK <jd.jdhp@gmail.com>
2023-04-10 01:06:04 +0200

    Add 2 snippets.

---
+++ python/pyside/pyside6/widget_QPushButton_clic_event_as_class.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# See http://doc.qt.io/qt-5/qpushbutton.html#details
+
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        button = QPushButton('Hello', self)
+        button.clicked.connect(self.on_clic)
+
+    def on_clic(self):
+        print("Hello!")
+
+
+app = QApplication(sys.argv)
+
+window = Window()
+window.show()
+
+exit_code = app.exec_()
+sys.exit(exit_code)
