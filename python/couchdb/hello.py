2087f0a4cc664dc438c24b77f63f649ab3b9f7ba
Jeremie DECOCK <jd.jdhp@gmail.com>
2024-06-07 14:42:15 +0200

    Fix a mistake in some comments.

--- python/couchdb/hello.py
+++ python/couchdb/hello.py
@@ -39,20 +39,20 @@ print(response.text)
 
 # Create a database ###########################################################
 
-# curl -X PUT http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@127.0.0.1:5984/test
+# curl -X PUT http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@127.0.0.1:5984/my_db
 response = requests.put(f'{DB_URL}/{DB_NAME}')
 print(response.text)
 
 
 # Push documents ##############################################################
 
-# curl -X POST -H "Content-Type: application/json" http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@127.0.0.1:5984/test -d '{ "in" : {"i1":1, "i2":2}, "out": {"o1":1, "o2":2} }'
+# curl -X POST -H "Content-Type: application/json" http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@127.0.0.1:5984/my_db -d '{ "in" : {"i1":1, "i2":2}, "out": {"o1":1, "o2":2} }'
 headers = {'Content-Type': 'application/json'}
 data = json.dumps({"in" : {"i1":1, "i2":2}, "out": {"o1":1, "o2":2}})
 response = requests.post(f'{DB_URL}/{DB_NAME}', headers=headers, data=data)
 print(response.text)
 
-# curl -X POST -H "Content-Type: application/json" http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@127.0.0.1:5984/test -d '{ "in" : {"i1":11, "i2":22}, "out": {"o1":11, "o2":22} }'
+# curl -X POST -H "Content-Type: application/json" http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@127.0.0.1:5984/my_db -d '{ "in" : {"i1":11, "i2":22}, "out": {"o1":11, "o2":22} }'
 data = json.dumps({"in" : {"i1":11, "i2":22}, "out": {"o1":11, "o2":22}})
 response = requests.post(f'{DB_URL}/{DB_NAME}', headers=headers, data=data)
 print(response.text)
@@ -61,13 +61,13 @@ print(response.text)
 # Query the database ##########################################################
 
 # Create a view
-# curl -X PUT http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@127.0.0.1:5984/test/_design/myview -d '{"views":{"my_filter":{"map": "function(doc) { if(doc.in && doc.in.i1 && doc.out) { emit(doc.in.i1, doc.out); }}"}}}'
+# curl -X PUT http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@127.0.0.1:5984/my_db/_design/my_view -d '{"views":{"my_filter":{"map": "function(doc) { if(doc.in && doc.in.i1 && doc.out) { emit(doc.in.i1, doc.out); }}"}}}'
 data = json.dumps({"views":{DB_FILTER:{"map": "function(doc) { if(doc.in && doc.in.i1 && doc.out) { emit(doc.in.i1, doc.out); }}"}}})
 response = requests.put(f'{DB_URL}/{DB_NAME}/_design/{DB_VIEW}', headers=headers, data=data)
 print(response.text)
 
 # Query the view
-# curl -X GET http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@127.0.0.1:5984/test/_design/myview/_view/my_filter
+# curl -X GET http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@127.0.0.1:5984/my_db/_design/my_view/_view/my_filter
 response = requests.get(f'{DB_URL}/{DB_NAME}/_design/{DB_VIEW}/_view/{DB_FILTER}')
 print(response.text)
 print(response.json())
