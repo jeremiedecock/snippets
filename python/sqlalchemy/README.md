4efe9625309c8c7452a8c8d8083d413fd555ffb2
Jeremie DECOCK <jd.jdhp@gmail.com>
2024-08-18 16:52:50 +0200

    Add a snippet.

---
+++ python/sqlalchemy/README.md
@@ -0,0 +1,3 @@
+# SQLAlchemy
+
+https://www.sqlalchemy.org/
---
+++ python/sqlalchemy/hello_sqlite.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# C.f. https://docs.sqlalchemy.org/en/20/tutorial/dbapi_transactions.html
+
+from sqlalchemy import create_engine, text
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+# "commit as you go"
+with engine.connect() as conn:
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+    )
+    conn.commit()
+
+# "begin once"
+with engine.begin() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
+    )
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table"))
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table WHERE y > :y"), {"y": 2})
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
