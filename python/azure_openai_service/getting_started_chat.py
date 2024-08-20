fd486bbff0f4a191888780229114a112f1b7e439
Jeremie DECOCK <jd.jdhp@gmail.com>
2024-08-20 19:55:08 +0200

    Update openai API.

--- python/azure_openai_service/getting_started_chat.py
+++ python/azure_openai_service/getting_started_chat.py
@@ -2,21 +2,49 @@
 
 # See: https://learn.microsoft.com/en-us/azure/cognitive-services/openai/quickstart?pivots=programming-language-python&tabs=command-line
 
-import openai
+from openai import AzureOpenAI
 
 from credentials import AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_API_KEY, AZURE_OPENAI_DEPLOYMENT_NAME
 
-openai.api_key = AZURE_OPENAI_API_KEY
-openai.api_base = AZURE_OPENAI_ENDPOINT     # Your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
-openai.api_type = 'azure'
-openai.api_version = '2023-05-15'           # This may change in the future
+client = AzureOpenAI(
+    azure_endpoint = AZURE_OPENAI_ENDPOINT,   # Your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
+    api_key=AZURE_OPENAI_API_KEY,  
+    api_version="2023-05-15" # model_dict["api_version"]
+)
 
-# Send a completion call to generate an answer
+# A chat test
 
-print('Sending a test completion job')
+PROMPT_STR = "Explain in a sentence in French what is backpropagation in neural networks."
 
-start_phrase = 'Write a tagline for an ice cream shop. '
-response = openai.Completion.create(engine=AZURE_OPENAI_DEPLOYMENT_NAME, prompt=start_phrase, max_tokens=10)
-text = response['choices'][0]['text'].replace('\n', '').replace(' .', '.').strip()
+print(PROMPT_STR)
 
-print(start_phrase + text)
+messages = [
+    {
+        "role": "system",
+        "content": "You are an AI assistant that helps people find information."
+    },
+    {
+        "role": "user",
+        "content": PROMPT_STR
+    }
+]
+
+try:
+    response = client.chat.completions.create(
+        model=AZURE_OPENAI_DEPLOYMENT_NAME,
+        messages = messages,
+        # temperature=0.7,
+        # max_tokens=800,
+        # top_p=0.95,
+        # frequency_penalty=0,
+        # presence_penalty=0,
+        # #stream=True,          # see https://www.gradio.app/guides/creating-a-chatbot-fast#a-streaming-example-using-openai
+        # stop=None
+    )
+
+    assistant_msg = response.choices[0].message.content
+    status = "success"
+except Exception as e:
+    print(f"chat error {e}")
+
+print("Answer:\n" + assistant_msg)
