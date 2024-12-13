5e2374f37af762cc355b0eaaff3c7c5dd81fdc01
Jeremie DECOCK <jd.jdhp@gmail.com>
2024-12-13 22:34:41 +0100

    Add a snippet.

---
+++ python/concurrent/hello_exception.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from multiprocessing import cpu_count
+import os
+import time
+
+NUM_CPU_CORES = cpu_count()
+
+
+def run_task(input_param: float):
+    print(f"Running task with input param {input_param} on process {os.getpid()}")
+    time.sleep(5)
+    raise Exception("This is an exception")
+
+
+def run_tasks() -> None:
+    input_params = [float(x) for x in range(32)]
+
+    with ProcessPoolExecutor(max_workers=NUM_CPU_CORES - 1) as executor:
+        futures = [executor.submit(run_task, input_param) for input_param in input_params]
+
+        for future in as_completed(futures):
+            try:
+                future.result()  # Ensure any exceptions are raised
+            except Exception as e:
+                print(f"Exception caught: {e}")
+                # executor.shutdown(wait=False)
+                break
+
+
+if __name__ == '__main__':
+    run_tasks()
