f89ee8fc9117f15c2f3d8e54f17958ded048ba9a
Jeremie DECOCK <jd.jdhp@gmail.com>
2025-03-05 01:13:58 +0100

    Add SmolLM2 + Podman instructions.

--- python/hugging_face_transformers/README.md
+++ python/hugging_face_transformers/README.md
@@ -8,9 +8,41 @@ See: https://github.com/huggingface/transformers
 From this directory:
 
 ```
-conda deactivate         # Only if you use Anaconda...
 python3 -m venv env
 source env/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 ```
+
+## Usage
+
+...
+
+## Podman
+
+### Build the Podman image
+
+```
+./build.sh
+```
+
+or
+
+```
+podman build -t snippets-hf-transformers:latest .
+```
+
+### Run a script using the Podman image
+
+```
+./run.sh python3 smollm2-135m-instruct.py
+```
+
+or 
+
+```
+podman run --rm -it -v .:/app -w /app -u $(id -u):$(id -g) --userns=keep-id localhost/snippets-hf-transformers:latest python3 smollm2-135m-instruct.py
+```
+
+To use Nvidia GPUs with Podman, check https://docs.nvidia.com/ai-enterprise/deployment/rhel-with-kvm/latest/podman.html#testing-podman-and-nvidia-container-runtime
+
---
+++ python/hugging_face_transformers/smollm2-135m-instruct.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# https://huggingface.co/HuggingFaceTB/SmolLM2-135M-Instruct
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+checkpoint = "HuggingFaceTB/SmolLM2-135M-Instruct"
+
+device = "cpu" # "cuda" for GPU usage
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+# for multiple GPUs install accelerate and do `model = AutoModelForCausalLM.from_pretrained(checkpoint, device_map="auto")`
+model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)
+
+messages = [{"role": "user", "content": "What is gravity?"}]
+
+input_text=tokenizer.apply_chat_template(messages, tokenize=False)
+print(input_text)
+
+inputs = tokenizer.encode(input_text, return_tensors="pt").to(device)
+outputs = model.generate(inputs, max_new_tokens=500, temperature=0.2, top_p=0.9, do_sample=True)
+
+print(tokenizer.decode(outputs[0]))
+
