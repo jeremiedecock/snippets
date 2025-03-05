c03c82741ec499d5883170023a602e92eeafc8c0
Jeremie DECOCK <jd.jdhp@gmail.com>
2025-03-05 23:39:25 +0100

    Fix a bug.

--- python/hugging_face_diffusers/sdxl-1.0-base_cpu.py
+++ python/hugging_face_diffusers/sdxl-1.0-base_cpu.py
@@ -5,7 +5,7 @@
 from diffusers import DiffusionPipeline
 import torch
 
-pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", use_safetensors=True)
 pipe.to("cpu")
 
 # if using torch < 2.0
