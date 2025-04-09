e052096e5439d59c5f7aedb01e082d57f7562c56
Jeremie DECOCK <jd.jdhp@gmail.com>
2025-04-09 12:29:39 +0200

    Improve a snippet.

--- python/langchain/app.py
+++ python/langchain/app.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
 
-from langchain.llms import HuggingFacePipeline
+# Documentation:
+# - https://huggingface.co/blog/langchain
+# - https://python.langchain.com/docs/integrations/providers/huggingface/
+
+from langchain_huggingface import HuggingFacePipeline
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 # Charger le modèle Hugging Face
 
-#model_name = "EleutherAI/phi-1.5"
-model_name = "microsoft/Phi-3.5-mini-instruct"
+model_name = "HuggingFaceTB/SmolLM2-135M-Instruct"
+# model_name = "HuggingFaceTB/SmolLM2-360M-Instruct"
+
+device = "cpu" # "cuda" for GPU usage
+
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
 
 # Configurer le pipeline
 
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=50)
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=100,
+    top_k=50,
+    do_sample=True,
+    temperature=0.5
+)
 llm = HuggingFacePipeline(pipeline=pipe)
 
 # Test de génération de texte
 
-prompt = "Hello, how are you?"
-output = llm(prompt)
+prompt = "HuggingFace is"
+output = llm.invoke(prompt)
 
 print(f"Input: {prompt}\nOutput: {output}")
 
