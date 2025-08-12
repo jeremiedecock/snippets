f5bc6284d110231aec58f785890b495db4855d12
Jeremie DECOCK <jd.jdhp@gmail.com>
2025-08-12 02:09:02 +0200

    Misc updates.

--- python/fastapi/path_and_query_params2.py
+++ python/fastapi/path_and_query_params2.py
@@ -1,17 +1,19 @@
 # Run with: fastapi dev path_and_query_params2.py --port 8000
 # then open http://localhost:8000/docs
 
+
 import fastapi
 
 app = fastapi.FastAPI()
 
-@app.get('/hello/{lastname}/{firsname}')
+@app.get('/hello/{lastname}/{firstname}')                       # 👈 *path paramètres* "firstname" et "lastname"
 def foo(
     firstname : str,
     lastname : str,
-    title : str | None = None
+    country : str,                                             # 👈 *query parameter* "country"
+    title : str | None = None                                  # 👈 *query parameter* "title"
 ) -> str:
     if title is None:
-        return f"Hello {firstname} {lastname}"
+        return f"Hello {firstname} {lastname} of {country}"
     else:
-        return f"Hello {title} {firstname} {lastname}"
+        return f"Hello {title} {firstname} {lastname} of {country}"
