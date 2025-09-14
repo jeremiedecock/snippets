7d8ca457eefbd9900c64d0d6b8609d08957f8bf8
Jeremie DECOCK <jd.jdhp@gmail.com>
2025-09-14 15:22:47 +0200

    Misc update.

--- python/pytest/test_hello_monkeypatch_function.py
+++ python/pytest/test_hello_monkeypatch_function.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from hello_monkeypatch_function import getssh
 
 
-# Mocked return function to replace Path.home. Always return '/abc'.
+# Mocked return function to replace Path.home. Always return '/home/foo'.
 def mock_path_home():
-    return Path("/foo")
+    return Path("/home/foo")
 
 
 def test_getssh(monkeypatch):
@@ -16,4 +16,4 @@ def test_getssh(monkeypatch):
     # Calling getssh() will use mock_path_home in place of Path.home
     # for this test with the monkeypatch.
     x = getssh()
-    assert x == Path("/foo/.ssh")
+    assert x == Path("/home/foo/.ssh")
