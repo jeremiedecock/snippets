7ef4b752638d3fd2618bdb327d203fa367082dd5
Jeremie DECOCK <jd.jdhp@gmail.com>
2026-01-11 22:57:41 +0100

    Add a snippet.

---
+++ python/ray_core/hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# C.f. https://docs.ray.io/en/latest/ray-overview/getting-started.html#ray-core-quickstart
+
+import ray
+
+ray.init()
+
+@ray.remote
+def f(x):
+    return x * x
+
+future_res1 = f.remote(1)
+future_res2 = f.remote(2)
+
+print(ray.get(future_res1))  # 1
+print(ray.get(future_res2))  # 4
