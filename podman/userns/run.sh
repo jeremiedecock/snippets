dd460491671b7dc1bb0c9204da0b97d7873f0c26
Jeremie DECOCK <jd.jdhp@gmail.com>
2026-04-01 17:53:06 +0200

    Misc updates.

--- podman/userns/run.sh
+++ podman/userns/run.sh
@@ -3,6 +3,8 @@
 CONTAINER_UID=1000
 CONTAINER_GID=1000
 
-podman run --rm -it -v .:/workspace \
-  --userns=keep-id:uid=${CONTAINER_UID},gid=${CONTAINER_GID} \
-  localhost/snippets-podman-userns:latest /bin/bash
+podman run --rm -it \
+    -v .:/workspace \
+    --userns=keep-id:uid=${CONTAINER_UID},gid=${CONTAINER_GID} \
+    localhost/snippets-podman-userns:latest \
+    /bin/bash
