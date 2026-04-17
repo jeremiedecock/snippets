120ea86db2054f258bd88f475567d5f8b2b1c137
Jeremie DECOCK <jd.jdhp@gmail.com>
2026-04-17 04:07:34 +0200

    Add vllm snippets.

---
+++ python/vllm/serve_qwen3-0.6b.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+podman run \
+    --device nvidia.com/gpu=all \
+    -v hf-cache:/root/.cache/huggingface \
+    --ipc=host \
+    --env HF_HUB_ENABLE_HF_TRANSFER=0 \
+    --env HF_HUB_DISABLE_XET=1 \
+    --env VLLM_ENABLE_CUDA_COMPATIBILITY=1 \
+    -p 8080:8000 \
+    docker.io/vllm/vllm-openai:v0.8.5 --model Qwen/Qwen3-0.6B
