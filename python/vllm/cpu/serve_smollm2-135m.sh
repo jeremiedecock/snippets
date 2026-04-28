8d328744d932d919cc0c6a7f36eebefd0b2f8436
Jeremie DECOCK <jd.jdhp@gmail.com>
2026-04-28 15:57:29 +0200

    Limit the KV cache size.

--- python/vllm/cpu/serve_smollm2-135m.sh
+++ python/vllm/cpu/serve_smollm2-135m.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Remarque: `-e VLLM_CPU_KVCACHE_SPACE=8` limite le KV cache à 8 Go
+
 podman run \
     --rm \
     --name=vllm-cpu \
     -v hf-cache:/root/.cache/huggingface \
     --ipc=host \
     -p 8080:8000 \
+    -e VLLM_CPU_KVCACHE_SPACE=8 \
     localhost/snippets-vllm-cpu vllm serve --model "HuggingFaceTB/SmolLM2-135M"
