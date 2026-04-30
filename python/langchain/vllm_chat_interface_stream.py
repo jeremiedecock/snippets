29122d7b0600ce9c369e32701e12588f6432d687
Jeremie DECOCK <jd.jdhp@gmail.com>
2026-04-30 11:52:29 +0200

    Add a snippet + switch on CPU by default.

---
+++ python/langchain/vllm_chat_interface_stream.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from langchain_openai import ChatOpenAI
+
+# API_URL = "http://localhost:8080/v1"
+API_URL = "http://host.containers.internal:8080/v1"  # Use this URL when this script is run within a devcontainer and vLLM is served in a Podman container
+
+model = ChatOpenAI(
+    base_url=API_URL,
+    api_key="",         # vLLM does not require an API key, but the OpenAI class requires this parameter, so we can just pass an empty string
+    model="Qwen/Qwen3-0.6B",
+    # temperature=0.7,
+    # max_tokens=128,
+)
+
+for token in model.stream("The capital of France is"):
+    print(token.content, end="", flush=True)
+
+print()
